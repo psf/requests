4a3f55997bc73d9ff8312cfae68a028728b49f66
Kenneth Reitz <me@kennethreitz.com>
2011-02-13 23:51:37 -0500

    a few test fixtures for now

--- test_requests.py
+++ test_requests.py
@@ -5,7 +5,43 @@ import unittest
 
 import requests
 
-print requests.get('http://kennethreitz.com').headers
+
+class RequestsTestSuite(unittest.TestCase):
+	"""Requests test cases."""
+	
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		"""Teardown."""
+		pass
+		
+	def test_invalid_url(self):
+		self.assertRaises(ValueError, requests.get, 'hiwpefhipowhefopw')
+
+	def test_HTTP_200_OK_GET(self):
+		r = requests.get('http://google.com')
+		self.assertEqual(r.status_code, 200)
+
+	def test_HTTPS_200_OK_GET(self):
+		r = requests.get('https://google.com')
+		self.assertEqual(r.status_code, 200)
+
+	def test_HTTP_200_OK_HEAD(self):
+		r = requests.head('http://google.com')
+		self.assertEqual(r.status_code, 200)
+
+	def test_HTTPS_200_OK_HEAD(self):
+		r = requests.head('https://google.com')
+		self.assertEqual(r.status_code, 200)
+
+	def test_AUTH_HTTPS_200_OK_GET(self):
+		auth = requests.AuthObject('requeststest', 'requeststest')
+		url = 'https://convore.com/api/account/verify.json'
+		r = requests.get(url, auth=auth)
+
+		self.assertEqual(r.status_code, 200)
 
 
-r = requests.Request()
+if __name__ == '__main__':
+	unittest.main()
