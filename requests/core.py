933cf49daa524bb1ea89e82ecbcb5c57d8ae03e2
Kenneth Reitz <me@kennethreitz.com>
2011-02-15 09:45:54 -0500

    v0.2.3

--- requests/core.py
+++ requests/core.py
@@ -34,8 +34,8 @@ from .packages.poster.streaminghttp import register_openers
 
 
 __title__ = 'requests'
-__version__ = '0.2.2'
-__build__ = 0x000202
+__version__ = '0.2.3'
+__build__ = 0x000203
 __author__ = 'Kenneth Reitz'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2011 Kenneth Reitz'
