a74e95478822e59c2ae659c6af77148530d36ff9
Kenneth Reitz <me@kennethreitz.com>
2011-02-25 07:21:11 -0500

    test improvements

--- test_requests.py
+++ test_requests.py
@@ -27,6 +27,20 @@ class RequestsTestSuite(unittest.TestCase):
         r = requests.get('https://google.com')
         self.assertEqual(r.status_code, 200)
 
+    def test_HTTP_200_OK_GET_WITH_PARAMS(self):
+
+        heads = {'User-agent': 'Mozilla/5.0'}
+        
+        r = requests.get('http://www.google.com/search', params={'q': 'test'}, headers=heads)
+        self.assertEqual(r.status_code, 200)
+
+    def test_HTTP_200_OK_GET_WITH_MIXED_PARAMS(self):
+
+        heads = {'User-agent': 'Mozilla/5.0'}
+
+        r = requests.get('http://google.com/search?test=true', params={'q': 'test'}, headers=heads)
+        self.assertEqual(r.status_code, 200)
+        
     def test_HTTP_200_OK_HEAD(self):
         r = requests.head('http://google.com')
         self.assertEqual(r.status_code, 200)
@@ -51,31 +65,44 @@ class RequestsTestSuite(unittest.TestCase):
     def test_POSTBIN_GET_POST_FILES(self):
         bin = requests.get('http://www.postbin.org/')
         self.assertEqual(bin.status_code, 200)
-
+    
         post = requests.post(bin.url, data={'some': 'data'})
         self.assertEqual(post.status_code, 200)
-
+    
         post2 = requests.post(bin.url, files={'some': open('test_requests.py')})
         self.assertEqual(post2.status_code, 200)
+        
+    def test_POSTBIN_GET_POST_FILES_WITH_PARAMS(self):
+        # TODO: postbin w/ params
+        bin = requests.get('http://www.postbin.org/')
+        self.assertEqual(bin.status_code, 200)
+    
+        post = requests.post(bin.url, data={'some': 'data'})
+        self.assertEqual(post.status_code, 200)
+    
+        # post2 = requests.post(bin.url, files={'some': open('test_requests.py')}, data={'some': 'data'})
+        # self.assertEqual(post2.status_code, 200)
 
     def test_nonzero_evaluation(self):
         r = requests.get('http://google.com/some-404-url')
         self.assertEqual(bool(r), False)
-
+    
         r = requests.get('http://google.com/')
         self.assertEqual(bool(r), True)
-
+    
     def test_request_ok_set(self):
         r = requests.get('http://google.com/some-404-url')
         self.assertEqual(r.ok, False)
-
+    
     def test_status_raising(self):
         r = requests.get('http://google.com/some-404-url')
         self.assertRaises(requests.HTTPError, r.raise_for_status)
-
+    
         r = requests.get('http://google.com/')
         self.assertFalse(r.error)
         r.raise_for_status()
+        
+
 
 
 if __name__ == '__main__':
