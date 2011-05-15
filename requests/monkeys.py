c40deefb8a192c83ceab0cfe745ed2bdf378130c
Kenneth Reitz <me@kennethreitz.com>
2011-05-14 23:03:31 -0400

    cleanup mess

--- requests/monkeys.py
+++ requests/monkeys.py
@@ -30,10 +30,7 @@ class Request(urllib2.Request):
 class HTTPRedirectHandler(urllib2.HTTPRedirectHandler):
 
     def http_error_301(self, req, fp, code, msg, headers):
-        # print "Cookie Manip Right Here"
-        # print 'HEY'
         pass
-        # return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
 
     http_error_302 = http_error_303 = http_error_307 = http_error_301
 
