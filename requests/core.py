86a06c5c184272ffd8e6fd4a33b427400713f739
Kenneth Reitz <me@kennethreitz.com>
2011-08-16 23:58:07 -0400

    auth_manager internal

--- requests/core.py
+++ requests/core.py
@@ -19,7 +19,7 @@ __license__ = 'ISC'
 __copyright__ = 'Copyright 2011 Kenneth Reitz'
 
 
-from models import HTTPError, auth_manager
+from models import HTTPError
 from api import *
 from exceptions import *
 from status_codes import codes
