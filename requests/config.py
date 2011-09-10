ccb9eb28e1c45e7b835f6d6fd74ff2e7e1a80eb2
Kenneth Reitz <me@kennethreitz.com>
2011-09-10 19:03:16 -0400

    slimming down the repo

--- requests/config.py
+++ requests/config.py
@@ -9,48 +9,11 @@ This module provides the Requests settings feature set.
 """
 
 class Settings(object):
-    _singleton = {}
-
-    # attributes with defaults
-    __attrs__ = []
 
     def __init__(self, **kwargs):
         super(Settings, self).__init__()
 
-        self.__dict__ = self._singleton
-
-
-    def __call__(self, *args, **kwargs):
-        # new instance of class to call
-        r = self.__class__()
-
-        # cache previous settings for __exit__
-        r.__cache = self.__dict__.copy()
-        map(self.__cache.setdefault, self.__attrs__)
-
-        # set new settings
-        self.__dict__.update(*args, **kwargs)
-
-        return r
-
-
-    def __enter__(self):
-        pass
-
-
-    def __exit__(self, *args):
-
-        # restore cached copy
-        self.__dict__.update(self.__cache.copy())
-        del self.__cache
-
-
     def __getattribute__(self, key):
-        if key in object.__getattribute__(self, '__attrs__'):
-            try:
-                return object.__getattribute__(self, key)
-            except AttributeError:
-                return None
         return object.__getattribute__(self, key)
 
 
