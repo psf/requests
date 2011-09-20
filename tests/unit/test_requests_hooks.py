35b72f5453a798330a3a48ba8f7a040786bd715e
Daniel Schauenberg <d@unwiredcouch.com>
2011-09-20 07:11:41 +0200

    check return values in hooks unit tests

--- tests/unit/test_requests_hooks.py
+++ tests/unit/test_requests_hooks.py
@@ -13,9 +13,13 @@ class RequestsHooksUnitTests(unittest.TestCase):
 
     def setUp(self):
         self.args_hook = mock.Mock()
+        self.args_hook.return_value = "args"
         self.pre_hook = mock.Mock()
+        self.pre_hook.return_value = "pre"
         self.post_hook = mock.Mock()
+        self.post_hook.return_value = "post"
         self.response_hook = mock.Mock()
+        self.response_hook.return_value = "response"
         self.hooks = { "args": self.args_hook,
                        "pre_request": self.pre_hook,
                        "post_request": self.post_hook,
@@ -27,20 +31,24 @@ class RequestsHooksUnitTests(unittest.TestCase):
         pass
 
     def test_dispatch_hook_args(self):
-        dispatch_hook("args", self.hooks, "called")
+        res = dispatch_hook("args", self.hooks, "called")
         self.args_hook.assert_called_once_with("called")
+        self.assertEqual("args", res)
 
     def test_dispatch_hook_pre_request(self):
-        dispatch_hook("pre_request", self.hooks, "called")
+        res = dispatch_hook("pre_request", self.hooks, "called")
         self.pre_hook.assert_called_once_with("called")
+        self.assertEqual("pre", res)
 
     def test_dispatch_hook_post_request(self):
-        dispatch_hook("post_request", self.hooks, "called")
+        res = dispatch_hook("post_request", self.hooks, "called")
         self.post_hook.assert_called_once_with("called")
+        self.assertEqual("post", res)
 
     def test_dispatch_hook_response(self):
-        dispatch_hook("response", self.hooks, "called")
+        res = dispatch_hook("response", self.hooks, "called")
         self.response_hook.assert_called_once_with("called")
+        self.assertEqual("response", res)
 
 if __name__ == '__main__':
     unittest.main()
