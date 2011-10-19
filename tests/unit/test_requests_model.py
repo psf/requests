ab3030a862399dfd3d7d233f0d9b54f73cf91d97
Daniel Schauenberg <d@unwiredcouch.com>
2011-10-19 10:28:07 +0200

    add test case for Request.__init__

--- tests/unit/test_requests_model.py
+++ tests/unit/test_requests_model.py
@@ -77,5 +77,24 @@ class RequestsAPIUnitTests(unittest.TestCase):
         self.assertEqual(HTTPForcedBasicAuthHandler, auth.handler)
         self.assertEqual('Snake Mountain', auth.realm)
 
+    def test_Request_init(self):
+        headers = {'Accept-Encoding': 'gzip', 'User-Agent': 'python-requests.org'}
+        r = models.Request(url="google.com", headers=dict(), files=None,
+                           method='get',
+                           data=dict(), params=dict(), auth=None, cookiejar=None,
+                           timeout=None, redirect=False, allow_redirects=False,
+                           proxies=None)
+
+        self.assertEqual('get',r.method)
+        self.assertEqual('google.com',r.url)
+        self.assertEqual(None,r.timeout)
+        self.assertEqual(None,r.files)
+        self.assertEqual([],r.data)
+        self.assertEqual([],r.params)
+        self.assertEqual(None,r.proxies)
+        self.assertEqual(headers,r.headers)
+        self.assertEqual(False,r.redirect)
+        self.assertEqual(False,r.allow_redirects)
+
 if __name__ == '__main__':
     unittest.main()
