d23c4b94c11eadfba7554fa7216f2a30b04396ca
Kenneth Reitz <me@kennethreitz.com>
2011-10-23 11:00:36 -0400

    httpbasic authentication cleanup

--- requests/auth.py
+++ requests/auth.py
@@ -7,15 +7,17 @@ requests.auth
 This module contains the authentication handlers for Requests.
 """
 
-from base64 import encodestring as base64
+from base64 import base64
 
 def http_basic(r, username, password):
     """Attaches HTTP Basic Authentication to the given Request object.
     Arguments should be considered non-positional.
 
     """
+    username = str(username)
+    password = str(password)
 
-    auth_s = base64('%s:%s' % (username, password)).replace('\n', '')
+    auth_s = base64('%s:%s' % (username, password))
     r.headers['Authorization'] = ('Basic %s' % auth_s)
 
     return r
