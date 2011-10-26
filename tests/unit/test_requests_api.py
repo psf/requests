06418610c53785af3f3770fb6cec85803a9d6ba5
Daniel Schauenberg <d@unwiredcouch.com>
2011-10-27 01:58:10 +0200

    adapt api unit tests to new structure

--- tests/unit/test_requests_api.py
+++ tests/unit/test_requests_api.py
@@ -21,41 +21,38 @@ class RequestsAPIUnitTests(unittest.TestCase):
         pass
 
 
-    @mock.patch('requests.api.dispatch_hook')
-    @mock.patch('requests.api.Request')
-    @mock.patch('requests.api.cookiejar_from_dict')
-    def test_request(self, mock_cjar, mock_request, mock_hook):
-        args = dict(
-                    method = None,
-                    url = None,
-                    data = None,
-                    params = None,
-                    headers = None,
-                    cookiejar = None,
-                    files = None,
-                    auth = None,
-                    timeout = 1,
-                    allow_redirects = None,
-                    proxies = None,
-                   )
-        hooks = {'args': args, 'pre_request': mock_request,
-                 'post_request': mock_request, 'response': 'response'}
-        sideeffect = lambda x,y,z: hooks[x]
-        mock_cjar.return_value = None
-        mock_request.send = mock.Mock(return_value={})
-        mock_request.response = "response"
-        mock_hook.side_effect = sideeffect
+    @mock.patch('requests.api.session')
+    def test_request(self, mock_session):
+        mock_request = mock_session.return_value
+        mock_request.request.return_value = "response"
+        r = requests.request('get','http://google.com',
+                             params=None,
+                             data=None,
+                             headers=None,
+                             cookies=None,
+                             files=None,
+                             auth=None,
+                             timeout=None,
+                             allow_redirects=False,
+                             proxies=None,
+                             hooks=None,
+                             return_response=True,
+                             config=None)
 
-        r = requests.request('get','http://google.com')
-
-
-        mock_cjar.assert_called_once_with({})
-        mock_hook.assert_called__with('args', None, args)
-        mock_request.assert_called_once_with(**args)
-        mock_hook.assert_called__with('pre_request', None, mock_request)
-        mock_request.send.assert_called_once_with()
-        mock_hook.assert_called__with('post_request', None, mock_request)
-        mock_hook.assert_called__with('response', None, mock_request)
+        mock_request.assert_called__with('get',
+                                         'http://google.com',
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         False,
+                                         None,
+                                         None,
+                                         True,
+                                         None)
         self.assertEqual(r, "response")
 
 
