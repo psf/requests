821448af77b3340a54e221326d03344647258440
Daniel Schauenberg <d@unwiredcouch.com>
2011-10-27 02:10:51 +0200

    remove AuthObject tests and adapt Request init test

--- tests/unit/test_requests_model.py
+++ tests/unit/test_requests_model.py
@@ -7,11 +7,6 @@ import os
 sys.path.append(os.getcwd())
 
 from requests import models
-from requests.monkeys import (HTTPBasicAuthHandler,
-                              HTTPForcedBasicAuthHandler,
-                              HTTPDigestAuthHandler,
-                              HTTPRedirectHandler)
-import urllib2
 
 class RequestsModelUnitTests(unittest.TestCase):
     """Requests API unit test cases."""
@@ -24,68 +19,12 @@ class RequestsModelUnitTests(unittest.TestCase):
         """Teardown."""
         pass
 
-    def test_AuthObject_defaults(self):
-        auth = models.AuthObject('Skeletor', 'mumble')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(HTTPForcedBasicAuthHandler, auth.handler)
-        self.assertEqual(None, auth.realm)
-
-    def test_AuthObject_forcedbasic_with_realm(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'forced_basic',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(HTTPForcedBasicAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
-    def test_AuthObject_basic_with_realm(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'basic',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(HTTPBasicAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
-    def test_AuthObject_digest_with_realm(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'digest',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(HTTPDigestAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
-    def test_AuthObject_proxybasic_with_realm(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'proxy_basic',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(urllib2.ProxyBasicAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
-    def test_AuthObject_proxdigest_with_realm(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'proxy_digest',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(urllib2.ProxyDigestAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
-    def test_AuthObject_with_bad_handler(self):
-        auth = models.AuthObject('Skeletor', 'mumble', 'panthor',
-                                 'Snake Mountain')
-        self.assertEqual('Skeletor', auth.username)
-        self.assertEqual('mumble', auth.password)
-        self.assertEqual(HTTPForcedBasicAuthHandler, auth.handler)
-        self.assertEqual('Snake Mountain', auth.realm)
-
     def test_Request_init(self):
-        headers = {'Accept-Encoding': 'gzip', 'User-Agent': 'python-requests.org'}
         r = models.Request(url="google.com", headers=dict(), files=None,
                            method='get',
-                           data=dict(), params=dict(), auth=None, cookiejar=None,
+                           data=dict(), params=dict(), auth=None, cookies=None,
                            timeout=None, redirect=False, allow_redirects=False,
-                           proxies=None)
+                           proxies=None, hooks=None, config=None)
 
         self.assertEqual('get',r.method)
         self.assertEqual('google.com',r.url)
@@ -94,9 +33,11 @@ class RequestsModelUnitTests(unittest.TestCase):
         self.assertEqual([],r.data)
         self.assertEqual([],r.params)
         self.assertEqual(None,r.proxies)
-        self.assertEqual(headers,r.headers)
+        self.assertEqual({},r.headers)
         self.assertEqual(False,r.redirect)
         self.assertEqual(False,r.allow_redirects)
+        self.assertEqual(None,r.hooks)
+        self.assertEqual(None,r.config)
 
 if __name__ == '__main__':
     unittest.main()
