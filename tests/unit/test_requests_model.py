efa97e1ed342a22212cba6bb9c413c35b8eb4b1e
Daniel Schauenberg <d@unwiredcouch.com>
2011-10-31 07:25:46 +0100

    add test for Request.__init__ with default args

--- tests/unit/test_requests_model.py
+++ tests/unit/test_requests_model.py
@@ -39,5 +39,20 @@ class RequestsModelUnitTests(unittest.TestCase):
         self.assertEqual(None,r.hooks)
         self.assertEqual(None,r.config)
 
+    def test_Request_init_no_args(self):
+        r = models.Request()
+        self.assertEqual(None,r.method)
+        self.assertEqual(None,r.url)
+        self.assertEqual(None,r.timeout)
+        self.assertEqual(None,r.files)
+        self.assertEqual([],r.data)
+        self.assertEqual([],r.params)
+        self.assertEqual(None,r.proxies)
+        self.assertEqual({},r.headers)
+        self.assertEqual(False,r.redirect)
+        self.assertEqual(False,r.allow_redirects)
+        self.assertEqual(None,r.hooks)
+        self.assertEqual(None,r.config)
+
 if __name__ == '__main__':
     unittest.main()
