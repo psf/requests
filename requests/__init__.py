f7257eada6ed60e5a09eb09487f3a71ee982f015
Kenneth Reitz <me@kennethreitz.com>
2011-11-09 17:30:20 -0800

    Session at root

--- requests/__init__.py
+++ requests/__init__.py
@@ -25,7 +25,7 @@ __copyright__ = 'Copyright 2011 Kenneth Reitz'
 from . import utils
 from .models import Request, Response
 from .api import request, get, head, post, patch, put, delete
-from .sessions import session
+from .sessions import session, Session
 from .status_codes import codes
 from .exceptions import (
     RequestException, Timeout, URLRequired,
