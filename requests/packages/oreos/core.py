e17155151b1af8fcd48300e3e5a8f11298fa0fe3
Kenneth Reitz <me@kennethreitz.com>
2011-11-19 15:42:36 -0500

    backup

--- requests/packages/oreos/core.py
+++ requests/packages/oreos/core.py
@@ -7,19 +7,18 @@ oreos.core
 The creamy white center.
 """
 
-from .structures import MultiDict
 from .monkeys import SimpleCookie
 
 
 def dict_from_string(s):
     """Returns a MultiDict with Cookies."""
 
-    cookies = MultiDict()
+    cookies = dict()
 
     c = SimpleCookie()
     c.load(s)
 
     for k,v in c.items():
-        cookies.add(k, v.value)
+        cookies.update({k: v.value})
 
     return cookies
