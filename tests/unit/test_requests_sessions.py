e99bba9fc001963ef4e3367c6dceffe5a910e9b7
Daniel Schauenberg <d@unwiredcouch.com>
2011-12-09 02:26:09 +0100

    add tests for HTTP methods in requests.sessions

--- tests/unit/test_requests_sessions.py
+++ tests/unit/test_requests_sessions.py
@@ -46,5 +46,132 @@ class RequestsSessionsUnitTests(unittest.TestCase):
         self.assertTrue(res['bar'])
         self.assertTrue('foo' not in res.keys())
 
+    @mock.patch('requests.sessions.PoolManager')
+    def test_sessions_session_init(self, mock_pool):
+        from requests.defaults import defaults
+        mock_pool.return_value = 'pool'
+        res = sessions.Session( headers=None,
+                                cookies=None,
+                                auth=None,
+                                timeout=None,
+                                proxies=None,
+                                hooks=None,
+                                params=None,
+                                config=None)
+        mock_pool.assert_called_once_with(num_pools=10, maxsize= 10)
+        self.assertEqual({}, res.headers)
+        self.assertEqual({}, res.cookies)
+        self.assertEqual(None, res.auth)
+        self.assertEqual(None, res.timeout)
+        self.assertEqual({}, res.proxies)
+        self.assertEqual({}, res.hooks)
+        self.assertEqual({}, res.params)
+        self.assertEqual(defaults, res.config)
+        self.assertEqual('pool', res.poolmanager)
+
+    @mock.patch('requests.sessions.PoolManager')
+    def test_sessions_session_init_with_cookies(self, mock_pool):
+        from requests.defaults import defaults
+        mock_pool.return_value = 'pool'
+        res = sessions.Session( headers=None,
+                               cookies={'foo':'bar'},
+                                auth=None,
+                                timeout=None,
+                                proxies=None,
+                                hooks=None,
+                                params=None,
+                                config=None)
+        mock_pool.assert_called_once_with(num_pools=10, maxsize= 10)
+        self.assertEqual({}, res.headers)
+        self.assertEqual(None, res.auth)
+        self.assertEqual(None, res.timeout)
+        self.assertEqual({}, res.proxies)
+        self.assertEqual({}, res.hooks)
+        self.assertEqual({}, res.params)
+        self.assertEqual(defaults, res.config)
+        self.assertEqual('pool', res.poolmanager)
+        self.assertEqual('bar', res.cookies['foo'])
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_request(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        res = sessions.Session().request( 'get', 'http://google.com',
+                                    params=None,
+                                    data=None,
+                                    headers=None,
+                                    cookies=None,
+                                    files=None,
+                                    auth=None,
+                                    timeout=None,
+                                    allow_redirects=False,
+                                    proxies=None,
+                                    hooks=None,
+                                    return_response=True,
+                                    config=None,
+                                    prefetch=False)
+
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_get(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.get('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_options(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.options('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_head(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.head('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_post(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.post('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_put(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.put('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_patch(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.patch('http://google.com')
+        self.assertEqual(ret.response, res)
+
+    @mock.patch('requests.sessions.Request')
+    def test_sessions_delete(self, mock_request):
+        ret = mock.MagicMock()
+        mock_request.return_value = ret
+        s = sessions.Session()
+        res = s.delete('http://google.com')
+        self.assertEqual(ret.response, res)
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
