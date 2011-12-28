a1afbfbaa3babf4f31e54cd1920b9c0c845db029
Kenneth Reitz <me@kennethreitz.com>
2011-12-28 03:01:00 -0500

    external tests

---
+++ test_requests_ext.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+import unittest
+
+import requests
+
+try:
+    import omnijson as json
+except ImportError:
+    import json
+
+
+class RequestsTestSuite(unittest.TestCase):
+    """Requests test cases."""
+
+    # It goes to eleven.
+    _multiprocess_can_split_ = True
+
+    def test_addition(self):
+        assert (1 + 1) == 2
+
+    def test_ssl_hostname_ok(self):
+        requests.get('https://github.com', verify=True)
+
+    def test_ssl_hostname_not_ok(self):
+        requests.get('https://kennethreitz.com', verify=False)
+
+        self.assertRaises(requests.exceptions.SSLError, requests.get, 'https://kennethreitz.com', verify=True)
+
+    def test_ssl_hostname_session_not_ok(self):
+
+        s = requests.session(verify=True)
+
+        s.get('https://kennethreitz.com', verify=False)
+
+        self.assertRaises(requests.exceptions.SSLError, s.get, 'https://kennethreitz.com')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
