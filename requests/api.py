0536e5b708bea4e1ef25930bd7b4a8cfe930d6ce
Frank Kumro <frank@frankkumro.com>
2012-01-12 22:16:31 -0500

    Added prefetch documentation for api

--- requests/api.py
+++ requests/api.py
@@ -32,6 +32,7 @@ def request(method, url, **kwargs):
     :param session: (optional) A :class:`Session` object to be used for the request.
     :param config: (optional) A configuration dictionary.
     :param verify: (optional) if ``True``, the SSL cert will be verified. A CA_BUNDLE path can also be provided.
+    :param prefetch: (optional) if ``True``, the response content will be immediately downloaded.
     """
 
     s = kwargs.pop('session') if 'session' in kwargs else sessions.session()
