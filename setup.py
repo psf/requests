8493d8329e9b606503923323af6417844508a744
Kenneth Reitz <me@kennethreitz.com>
2012-01-23 02:28:01 -0500

    PYTHON 3

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
     ),
