84d9a1b577b69bcf5af18c232990b5a9c7d4dec3
Kenneth Reitz <me@kennethreitz.com>
2012-03-13 16:39:51 -0700

    dict_to_sequence

--- requests/sessions.py
+++ requests/sessions.py
@@ -16,6 +16,15 @@ from .utils import header_expand
 from .packages.urllib3.poolmanager import PoolManager
 
 
+def dict_to_sequence(d):
+    """Returns an internal sequence dictionary update."""
+
+    if hasattr(d, 'items'):
+        d = d.items()
+
+    return d
+
+
 def merge_kwargs(local_kwarg, default_kwarg):
     """Merges kwarg dictionaries.
 
