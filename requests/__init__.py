775b6f6f0098ffa9edd3874b1af4b98378377211
Kenneth Reitz <me@kennethreitz.com>
2012-03-30 23:42:56 -0400

    v0.11.1

--- requests/__init__.py
+++ requests/__init__.py
@@ -15,8 +15,8 @@ requests
 """
 
 __title__ = 'requests'
-__version__ = '0.11.0'
-__build__ = 0x001100
+__version__ = '0.11.1'
+__build__ = 0x001101
 __author__ = 'Kenneth Reitz'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2012 Kenneth Reitz'
