04c329d703784f4d44bb9472f51e7723caabd264
Kenneth Reitz <me@kennethreitz.com>
2012-03-31 02:53:14 -0400

    license=open("LICENSE").read(

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     package_data={'': ['LICENSE', 'NOTICE']},
     include_package_data=True,
     install_requires=required,
-    license='ISC',
+    license=open("LICENSE").read(
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
