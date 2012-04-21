ed7b14899fcc196acb5631819585dcc2be1b41f7
barberj <barber.justin@gmail.com>
2012-04-20 22:15:35 -0400

    added mutlipart file=strings

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -329,6 +329,21 @@ class RequestsTestSuite(TestSetup, TestBaseMixin, unittest.TestCase):
 
             self.assertEqual(post2.status_code, 200)
 
+    def test_POSTBIN_GET_POST_FILES_STRINGS(self):
+
+        for service in SERVICES:
+
+            url = service('post')
+
+            post1 = post(url, files={'fname.txt': 'fdata'})
+            self.assertEqual(post1.status_code, 200)
+
+            post2 = post(url, files={'fname.txt': 'fdata', 'fname2.txt':'more fdata'})
+            self.assertEqual(post2.status_code, 200)
+
+            post3 = post(url, files={'fname.txt': 'fdata', 'fname2.txt':open(__file__,'rb')})
+            self.assertEqual(post3.status_code, 200)
+
     def test_nonzero_evaluation(self):
 
         for service in SERVICES:
