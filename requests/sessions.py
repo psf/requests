973e9cb54826062328f1c8650630819205ccfe8e
Suraj Barkale <suraj@barkale.com>
2012-05-15 19:47:02 +0530

    Remove sharing of base_headers config between sessions. Since base_headers is a dictionary, it's reference was shared with all instances of Session objects. Using deepcopy here should not incur large performance penalty.

--- requests/sessions.py
+++ requests/sessions.py
@@ -9,6 +9,7 @@ requests (cookies, auth, proxies).
 
 """
 
+from copy import deepcopy
 from .compat import cookielib
 from .cookies import cookiejar_from_dict, remove_cookie_by_name
 from .defaults import defaults
@@ -81,7 +82,7 @@ class Session(object):
         self.cert = cert
 
         for (k, v) in list(defaults.items()):
-            self.config.setdefault(k, v)
+            self.config.setdefault(k, deepcopy(v))
 
         self.init_poolmanager()
 
