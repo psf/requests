12328d6e58d0b52f0a5f425f4dcd84236583c679
Marcus McCurdy <marcus.mccurdy@gmail.com>
2012-08-03 20:09:56 -0400

    Added a test to expose issue #747

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -973,5 +973,12 @@ class RequestsTestSuite(TestSetup, TestBaseMixin, unittest.TestCase):
         self.assertEqual(t.get('form'), {'field': 'a, b'})
         self.assertEqual(t.get('files'), files)
 
+    def test_str_data_content_type(self):
+        data = "test string data"
+        r = post(httpbin('post'), data=data)
+        t = json.loads(r.text)
+        self.assertEqual(t.get('headers').get('Content-Type'), 'text/plain')
+
+
 if __name__ == '__main__':
     unittest.main()
