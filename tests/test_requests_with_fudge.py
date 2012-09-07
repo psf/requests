1e8feebb4f635e11aefd3dab2dc789cf02ad1159
barberj <barber.justin@gmail.com>
2012-09-06 22:14:47 -0400

    better documentation of test

--- tests/test_requests_with_fudge.py
+++ tests/test_requests_with_fudge.py
@@ -12,7 +12,10 @@ class TestRequestsWithFudge(unittest.TestCase):
         'requests.models.extract_cookies_to_jar')
     def test_requests_build_response(self, fake_pm, fake_c):
         """
-        Show that header defaulting to None is incorrect.
+        Fake HTTPResponse has a header attr set to None.
+        This was equivalent to the getattr(resp, 'headers', None).
+
+        TypeError is raised.
         """
         fake_pm.is_callable().returns_fake().is_a_stub().\
             expects('connection_from_url').returns_fake().\
@@ -22,10 +25,15 @@ class TestRequestsWithFudge(unittest.TestCase):
         try:
             requests.get('http://httpbin.org')
             assert False, 'Failed to raise exception'
-        except Exception as e:
+        except TypeError as e:
             assert e.args[0] == "'NoneType' object is not iterable"
+        except Exception:
+            assert False, "Unexpected exception"
 
-        # and now returning a dictionary
+        """
+        Fake HTTPResponse has lacks headers attr
+        so default {} will be returned.
+        """
         fake_pm.is_callable().returns_fake().is_a_stub().\
             expects('connection_from_url').returns_fake().\
             expects('urlopen').returns_fake().has_attr(status=200)
