d09357574b3fc1c2b7a2188e3b62ef52db224791
David Andrzejewski <site+github.com@davidandrzejewski.com>
2012-10-22 08:37:59 -0400

    The previous revision could crash if Python is compiled without the SSL module.  Fixed.

--- requests/packages/urllib3/connectionpool.py
+++ requests/packages/urllib3/connectionpool.py
@@ -76,7 +76,7 @@ class VerifiedHTTPSConnection(HTTPSConnection):
     """
     cert_reqs = None
     ca_certs = None
-    ssl_version = ssl.PROTOCOL_SSLv23
+    ssl_version = None
 
     def set_cert(self, key_file=None, cert_file=None,
                  cert_reqs='CERT_NONE', ca_certs=None):
@@ -495,8 +495,8 @@ class HTTPSConnectionPool(HTTPConnectionPool):
     :class:`.VerifiedHTTPSConnection` is used, which *can* verify certificates,
     instead of :class:httplib.HTTPSConnection`.
 
-    The ``key_file``, ``cert_file``, ``cert_reqs``, and ``ca_certs`` parameters
-    are only used if :mod:`ssl` is available and are fed into
+    The ``key_file``, ``cert_file``, ``cert_reqs``, ``ca_certs``, and ``ssl_version``
+    parameters are only used if :mod:`ssl` is available and are fed into
     :meth:`ssl.wrap_socket` to upgrade the connection socket into an SSL socket.
     """
 
@@ -506,7 +506,7 @@ class HTTPSConnectionPool(HTTPConnectionPool):
                  strict=False, timeout=None, maxsize=1,
                  block=False, headers=None,
                  key_file=None, cert_file=None,
-                 cert_reqs='CERT_NONE', ca_certs=None, ssl_version=ssl.PROTOCOL_SSLv23):
+                 cert_reqs='CERT_NONE', ca_certs=None, ssl_version=None):
 
         super(HTTPSConnectionPool, self).__init__(host, port,
                                                   strict, timeout, maxsize,
@@ -536,7 +536,10 @@ class HTTPSConnectionPool(HTTPConnectionPool):
         connection.set_cert(key_file=self.key_file, cert_file=self.cert_file,
                             cert_reqs=self.cert_reqs, ca_certs=self.ca_certs)
 
-        connection.ssl_version = self.ssl_version
+        if self.ssl_version is None:
+            connection.ssl_version = ssl.PROTOCOL_SSLv23
+        else:
+            connection.ssl_version = self.ssl_version
 
         return connection
 
