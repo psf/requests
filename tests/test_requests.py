2365e6c4ba4bcd5007ec3668787723d2c36ea08f
Ryan J Daw <ryan@mobify.me>
2012-10-26 15:01:57 -0700

    Fixes problem with URIs converted from IRI
    The problem occurs when a IRI has been converted to an URI
    and contains percent-quoted characters. When this URI
    is again encoded with IDNA it fails because its length
    exceeds 64 characters.
    
    The solution is to detect percent-quote encoded domains,
    reversing the encoding, and then encoding with IDNA.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1202,8 +1202,8 @@ class RequestsTestSuite(TestSetup, TestBaseMixin, unittest.TestCase):
         RFC 3987: http://tools.ietf.org/html/rfc3987.html
         """
 
-        iri = u"http://испытаниепроверка.рф"
-
+        
+        # IRI u"http://испытаниепроверка.рф"
         # Corresponding URI.
         uri = 'http://%D0%B8%D1%81%D0%BF%D1%8B%D1%82%D0%B0%D0%BD%D0%B8%D0%B5%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%BA%D0%B0.%D1%80%D1%84'
 
