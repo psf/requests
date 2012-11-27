400046f9116ccee8baff0c5936a2ede530589fec
Kenneth Reitz <me@kennethreitz.com>
2012-11-27 12:37:59 -0800

    that shouldn't be neccesary anymore

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
-
 packages = [
     'requests',
     'requests.packages',
@@ -58,5 +56,3 @@ setup(
         'Programming Language :: Python :: 3.3',
     ),
 )
-
-del os.environ['PYTHONDONTWRITEBYTECODE']
