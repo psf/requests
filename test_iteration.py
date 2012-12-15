ec5844a3bca014f71673da091c8717f114c921f0
Kenneth Reitz <me@kennethreitz.com>
2012-12-15 00:44:42 -0500

    test

---
+++ test_iteration.py
@@ -0,0 +1,25 @@
+import requests
+import unittest
+
+
+class IterationTestCase(unittest.TestCase):
+
+    def test_assertion(self):
+        assert 1
+
+    # def test_dzubia(self):
+    #     s = requests.Session()
+    #     r = requests.Request(method='GET', url='http://github.com/')
+
+    #     # r = s.send(r)
+
+    def test_prepared_request(self):
+        s = requests.Session()
+        r = requests.Request(method='GET', url='http://github.com/')
+        r = r.prepare()
+
+        s.send(r)
+
+
+if __name__ == '__main__':
+    unittest.main()
