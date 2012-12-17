82cce0212064719b80fb26628249d6b1f2ced6b7
Kenneth Reitz <me@kennethreitz.com>
2012-12-17 07:29:33 -0500

    basic test

---
+++ test_requests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for Requests."""
+
+import unittest
+
+import requests
+
+
+class RequestsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Create simple data set with headers."""
+        pass
+
+    def tearDown(self):
+        """Teardown."""
+        pass
+
+    def test_assertion(self):
+        assert 1
+
+
+
