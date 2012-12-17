88b5ebceb61d65197ada96a8b2e42ef1afa31af8
Kenneth Reitz <me@kennethreitz.com>
2012-12-17 09:59:05 -0500

    no 3.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
