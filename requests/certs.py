b82a8aa183942566e2a71530994685cf16ff72c1
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2012-12-17 23:54:40 -0800

    restore old certificate bundle behavior
    Since a CA bundle started being vendored inside Requests, it always
    overrides the OS bundle. This changes the preference order to be
    packaged certifi, then the OS bundle, then the vendored bundle,
    allowing managed environments to use their system bundles by simply
    omitting a certifi package.

--- requests/certs.py
+++ requests/certs.py
@@ -2,26 +2,56 @@
 # -*- coding: utf-8 -*-
 
 """
-ceritfi.py
-~~~~~~~~~~
+certs.py
+~~~~~~~~
 
-This module returns the installation location of cacert.pem.
+This module returns the preferred default CA certificate bundle.
+
+Preference order:
+    1. The certifi Python package, if available
+    2. The operating system's bundle, if available
+    3. The vendored bundle inside Requests
 """
 
-import os
+import os.path
+
+certifi = None
 try:
     import certifi
 except ImportError:
-    certifi = None
+    pass
 
+# common paths for the OS's CA certificate bundle
+POSSIBLE_CA_BUNDLE_PATHS = [
+        # Red Hat, CentOS, Fedora and friends (provided by the ca-certificates package):
+        '/etc/pki/tls/certs/ca-bundle.crt',
+        # Ubuntu, Debian, and friends (provided by the ca-certificates package):
+        '/etc/ssl/certs/ca-certificates.crt',
+        # FreeBSD (provided by the ca_root_nss package):
+        '/usr/local/share/certs/ca-root-nss.crt',
+        # openSUSE (provided by the ca-certificates package), the 'certs' directory is the
+        # preferred way but may not be supported by the SSL module, thus it has 'ca-bundle.pem'
+        # as a fallback (which is generated from pem files in the 'certs' directory):
+        '/etc/ssl/ca-bundle.pem',
+]
+
+def get_os_ca_bundle_path():
+    """Try to pick an available CA certificate bundle provided by the OS."""
+    for path in POSSIBLE_CA_BUNDLE_PATHS:
+        if os.path.exists(path):
+            return path
+    return None
 
 def where():
-
+    """Return the preferred certificate bundle."""
     if certifi:
         return certifi.where()
-    else:
-        f = os.path.split(__file__)[0]
-        return os.path.join(f, 'cacert.pem')
+
+    os_bundle_path = get_os_ca_bundle_path()
+    if os_bundle_path:
+        return os_bundle_path
+
+    return os.path.join(os.path.dirname(__file__), 'cacert.pem')
 
 if __name__ == '__main__':
     print(where())
