d17b0ca04283a7c0fd2ce99e02eb2638523856cc
Jayson Vantuyl <jayson@gogrid.com>
2013-01-17 18:24:00 -0800

    added 'file' url-scheme support
    Adds file://... support.  It's unnaturally complete for what it does.
    
     * methods for GET / HEAD / PUT / POST / DELETE
     * turns symlinks into redirects
     * appropriate HTTP errors for access-denied, missing file, etc.
     * streaming support for large files
     * guesses content-types / encodings from filenames
     * date, last-modified, content-length, and other headers
    
    Since this is potentially dangerous, it's not enabled by default.  A few
    rudimentary tests are provided.
    
    For those who are wondering why anyone would want this, I use it for
    bootstrapping some of our HTTP clients (i.e. I have a list of URLs for
    fall-back, with a file://... URL at the end) and of testing (i.e. it's easier
    to just change the API base URI to file: than it is to mock out requests
    everywhere).

--- requests/adapters.py
+++ requests/adapters.py
@@ -8,7 +8,17 @@ This module contains the transport adapters that Requests uses to define
 and maintain connections.
 """
 
+from cgi import escape as html_escape
+from email.utils import formatdate
+import errno
+from mimetypes import guess_type as guess_mime_type
+from os import listdir, readlink, stat, strerror, unlink
+from os.path import exists
 import socket
+from stat import S_ISDIR, S_ISLNK, S_ISREG
+from StringIO import StringIO
+from traceback import format_exc
+from urllib import quote as url_quote, unquote as url_unquote
 
 from .models import Response
 from .packages.urllib3.poolmanager import PoolManager, proxy_from_url
@@ -222,3 +232,116 @@ class HTTPAdapter(BaseAdapter):
             r.content
 
         return r
+
+
+class FileAdapter(BaseAdapter):
+    def send(self, request, stream=False, verify=None, cert=None, proxies=None, timeout=None):
+        # TODO: conditional requests?
+        # TODO: idempotent put?
+        # TODO: range requests?
+        # TODO: logging?
+        # TODO: other verbs, OPTIONS, etc.
+        # TODO: content-type/encoding negotiation
+
+        if stream:
+            raise NotImplementedError("streaming not implemented")
+
+        fname = url_unquote(request.url[len('file://'):])
+        response = Response()
+        response.url = request.url
+        response.headers['Date'] = formatdate(timeval=None, localtime=True)
+
+        try:
+            if request.method in ('GET', 'HEAD'):
+                statdata = stat(fname)
+                etag = '"%s/%s/%s' % (statdata.st_dev, statdata.st_ino, statdata.st_mtime)
+                if S_ISLNK(statdata.st_mode):
+                    response.status_code = 302
+                    response.headers['Status'] = '302 Found'
+                    response.headers['Location'] = url_quote('file://' + readlink(fname))
+                elif S_ISDIR(statdata.st_mode):
+                    response.status_code = 200
+                    response.headers['Status'] = '200 Ok'
+                    body = """<html><head><title>%s</title></head><body><ul>""" % fname
+                    for subfname in sorted(listdir(fname)):
+                        body += '<li><a href="file://' + url_quote(subfname) + '">' + \
+                                html_escape(fname) + '</a></li>'
+                    body += '</body></html>'
+                    response.headers['ETag'] = 'W/' + etag
+                    response.raw = StringIO(body)
+                elif S_ISREG(statdata.st_mode):
+                    content = open(fname, 'r').read()
+                    response.status_code = 200
+                    response.headers['Content-Length'] = statdata.st_size
+                    response.headers['Last-Modified'] = formatdate(
+                        timeval=statdata.st_mtime,
+                        localtime=True
+                    )
+                    mt, enc = guess_mime_type(request.url, strict=False)
+                    if mt is None:
+                        mt = 'application/octet-stream'
+                    if enc is not None:
+                        response.headers['Content-Encoding'] = enc
+                    response.headers['Content-Type'] = mt
+                    response.headers['ETag'] = etag
+                    if request.method == 'GET':
+                        response.raw = open(fname, 'r')
+                else:
+                    response.status_code = 500
+                    response.headers['Status'] = '500 Internal Server Error'
+            elif request.method == 'PUT':
+                 if exists(fname):  # FIXME: Is this right?
+                     response.status_code = 409
+                     response.headers['Status'] = '409 Conflict'
+                 else:
+                     open(fname, 'w').write(request.body)
+            elif request.method == 'POST':
+                 open(fname, 'w').write(request.body)  # FIXME: Is this right?
+                 response.status_code = 200
+                 response.headers['Status'] = '200 Ok'
+            elif request.method == 'DELETE':
+                 unlink(fname)
+                 response.status_code = 200
+                 response.headers['Status'] = '200 Ok'
+            else:
+                response.status_code = 405
+                response.headers['Status'] = '405 Method Not Allowed'
+        except OSError, e:
+            if e.errno == errno.ENOENT:
+               if request.method == 'DELETE':
+                   response.status_code = 410
+                   response.headers['Status'] = '410 Gone'
+               else:
+                   response.status_code = 404
+                   response.headers['Status'] = '404 Not Found'
+            elif e.errno == errno.EISDIR:
+               response.status_code = 405
+               response.headers['Status'] = '405 Method Not Allowed'
+               response.body = 'Cannot %r a directory...' % request.method
+            elif e.errno == errno.EACCES:
+               response.status_code = 403
+               response.headers['Status'] = '403 Forbidden'
+            else:
+               response.status_code = 500
+               response.headers['Status'] = '500 Internal Server Error'
+               response.body = 'OSError: ' + strerror(e.errno)
+        except Exception:
+            response.status_code = 500
+            response.headers['500 Internal Server Error']
+            response.body = format_exc()
+
+        # context
+        response.request = request
+        response.connection = self
+
+        # hooks    
+        response = dispatch_hook('response', request.hooks, response)
+
+        # streaming
+        if not stream:
+            response.content
+
+        return response
+
+    def close(self):
+        pass
