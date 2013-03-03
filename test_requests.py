b14584f36eca344ed5033d1d2f5a59490fadca56
Kenneth Reitz <me@kennethreitz.com>
2013-03-03 12:01:47 -0500

    Revert "[kennethreitz/requests#1208] adding unit test for max_retries"
    This reverts commit 18b29ea00563a0bf8e15666806117444c87ac23f.

--- test_requests.py
+++ test_requests.py
@@ -366,26 +366,5 @@ class RequestsTestCase(unittest.TestCase):
         r = requests.Request(url=HTTPBIN)
         self.assertRaises(ValueError, requests.Session().send, r)
 
-    def test_can_specify_retries(self):
-        # monkey patch urlopen
-        from requests.packages.urllib3.poolmanager import HTTPConnectionPool
-        old_urlopen = HTTPConnectionPool.urlopen
-
-        max_retries_used = []
-        def urlopen(*args, **kwargs):
-            """Save what value we used for retries each time we call urlopen."""
-            max_retries_used.append(kwargs.get('retries'))
-            return old_urlopen(*args, **kwargs)
-
-        HTTPConnectionPool.urlopen = urlopen
-
-        # do the request and check that max_retries was passed through
-        requests.get(httpbin('get'), max_retries=5)
-        self.assertEqual(max_retries_used, [5])
-
-        # undo monkey patch
-        HTTPConnectionPool.urlopen = old_urlopen
-        
-
 if __name__ == '__main__':
     unittest.main()
