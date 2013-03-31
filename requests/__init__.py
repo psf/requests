d06908d655eca4467521c28d93a68d329d48d524
Kenneth Reitz <me@kennethreitz.com>
2013-03-31 08:28:22 +0300

    v1.2.0

--- requests/__init__.py
+++ requests/__init__.py
@@ -42,8 +42,8 @@ is at <http://python-requests.org>.
 """
 
 __title__ = 'requests'
-__version__ = '1.1.0'
-__build__ = 0x010100
+__version__ = '1.2.0'
+__build__ = 0x010200
 __author__ = 'Kenneth Reitz'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Kenneth Reitz'
