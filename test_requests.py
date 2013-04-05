5c47ce1136ed12e992fe3404b8e6634060bcd411
Chase Sterling <chase.sterling@gmail.com>
2013-04-04 22:48:14 -0400

    Make sure unit test works on python 2.6

--- test_requests.py
+++ test_requests.py
@@ -135,7 +135,7 @@ class RequestsTestCase(unittest.TestCase):
                     'foo=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT'
             }
         )
-        self.assertNotIn('foo', s.cookies)
+        assert 'foo' not in s.cookies
 
     def test_user_agent_transfers(self):
 
