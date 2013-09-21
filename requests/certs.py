2c62e09c64c52971856c34754c74f9d0d9d7b03c
Kyle <kyle.rockman@mac.com>
2013-09-21 09:06:02 -0500

    use resource_filename over resource_string
    due to errors with it being inside .egg and unusable by outside methods that need access to the data.  Per the PEP 302 really the data should be accessed by pkgutils.get_data() but that does not work in this case because of how the cacert.pem is being used.

--- requests/certs.py
+++ requests/certs.py
@@ -12,13 +12,15 @@ environment, you can change the definition of where() to return a separately
 packaged CA bundle.
 """
 
-from pkg_resources import resource_string
+#resource_filename is used over resource_string because the method that uses this needs access to the file
+#and since this file lives withing the .egg file, it needs to be temporarily extracted to be used
+from pkg_resources import resource_filename
 
 
 def where():
     """Return the preferred certificate bundle."""
     # vendored bundle inside Requests
-    return resource_string(__name__, "cacert.pem")
+    return resource_filename(__name__, "cacert.pem")
 
 if __name__ == '__main__':
     print(where())
