59b29d02f8c4674a9922df53c9df9f960e6eaf34
Kamil Madac <kamil.madac@gmail.com>
2013-12-04 15:11:10 +0100

    Tiny comment fix

--- requests/utils.py
+++ requests/utils.py
@@ -423,7 +423,7 @@ def address_in_network(ip, net):
 def dotted_netmask(mask):
     """
     Converts mask from /xx format to xxx.xxx.xxx.xxx
-    Example: if mask is /24 function returns 255.255.255.0
+    Example: if mask is 24 function returns 255.255.255.0
     """
     bits = 0xffffffff ^ (1 << 32 - mask) - 1
     return socket.inet_ntoa(struct.pack('>I', bits))
