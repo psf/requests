626c48e9471d8075039aeb5daefa61756951e03f
Donald Stufft <donald@stufft.io>
2014-05-12 14:43:50 -0400

    Update urllib3 to 4fb351cd2

--- requests/packages/urllib3/fields.py
+++ requests/packages/urllib3/fields.py
@@ -15,7 +15,7 @@ def guess_content_type(filename, default='application/octet-stream'):
     Guess the "Content-Type" of a file.
 
     :param filename:
-        The filename to guess the "Content-Type" of using :mod:`mimetimes`.
+        The filename to guess the "Content-Type" of using :mod:`mimetypes`.
     :param default:
         If no "Content-Type" can be guessed, default to `default`.
     """
