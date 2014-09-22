f08e4445a21398293e908fb16c2c18e9038995bb
Tim Konick <konick781@gmail.com>
2014-09-22 12:46:07 -0400

    rm `else` after `if` then `raise` block

--- requests/models.py
+++ requests/models.py
@@ -667,9 +667,8 @@ class Response(object):
         if self._content_consumed and isinstance(self._content, bool):
             raise RuntimeError(
                         'The content for this response was already consumed')
-        else:
-            # simulate reading small chunks of the content
-            reused_chunks = iter_slices(self._content, chunk_size)
+        # simulate reading small chunks of the content
+        reused_chunks = iter_slices(self._content, chunk_size)
 
         stream_chunks = generate()
 
