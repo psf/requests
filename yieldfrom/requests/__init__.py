548bc4a3e9a883dc66ad28ee26437850f0f6b3ed
David <dkeeney@travelbyroad.net>
2014-10-19 10:51:50 -0600

    change in version number

--- yieldfrom/requests/__init__.py
+++ yieldfrom/requests/__init__.py
@@ -43,7 +43,7 @@ is at <http://python-requests.org>.
 
 __title__ = 'yieldfrom.requests'
 __version__ = '0.1.1'
-__build__ = 0x020403
+__build__ = 0x000101
 __author__ = 'Kenneth Reitz'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2014 Kenneth Reitz'
