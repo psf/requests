025e5c859e69e24e57c9f60db47433c3fceacfa1
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-02 01:03:58 +0100

    Improving format (whitespace related PEP8).

--- requests/api.py
+++ requests/api.py
@@ -118,7 +118,7 @@ def patch(url, data=None, **kwargs):
     :param \*\*kwargs: Optional arguments that ``request`` takes.
     """
 
-    return request('patch', url,  data=data, **kwargs)
+    return request('patch', url, data=data, **kwargs)
 
 
 def delete(url, **kwargs):
--- requests/models.py
+++ requests/models.py
@@ -36,11 +36,11 @@ from .status_codes import codes
 #: The set of HTTP status codes that indicate an automatically
 #: processable redirect.
 REDIRECT_STATI = (
-    codes.moved,              # 301
-    codes.found,              # 302
-    codes.other,              # 303
-    codes.temporary_redirect, # 307
-    codes.permanent_redirect, # 308
+    codes.moved,               # 301
+    codes.found,               # 302
+    codes.other,               # 303
+    codes.temporary_redirect,  # 307
+    codes.permanent_redirect,  # 308
 )
 DEFAULT_REDIRECT_LIMIT = 30
 CONTENT_CHUNK_SIZE = 10 * 1024
