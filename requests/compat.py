e99a8ec0f351af6a8383e3380e1a5bb86d013bc6
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-03-24 15:14:42 +0200

    Import the builtin json first.

--- requests/compat.py
+++ requests/compat.py
@@ -22,11 +22,9 @@ is_py2 = (_ver[0] == 2)
 is_py3 = (_ver[0] == 3)
 
 try:
-    import simplejson as json
-except (ImportError, SyntaxError):
-    # simplejson does not support Python 3.2, it throws a SyntaxError
-    # because of u'...' Unicode literals.
     import json
+except ImportError:
+    import simplejson as json
 
 # ---------
 # Specifics
