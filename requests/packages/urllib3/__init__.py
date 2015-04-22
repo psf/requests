3d339ed7e61261832347190185113b890b55009e
Cory Benfield <lukasaoz@gmail.com>
2015-04-22 18:39:40 +0100

    Update urllib3 to 0b744993
    (cherry picked from commit df63ee3e13672caa6b3256c6579fa85caefb2729)

--- requests/packages/urllib3/__init__.py
+++ requests/packages/urllib3/__init__.py
@@ -4,7 +4,7 @@ urllib3 - Thread-safe connection pooling and re-using.
 
 __author__ = 'Andrey Petrov (andrey.petrov@shazow.net)'
 __license__ = 'MIT'
-__version__ = '1.10.2'
+__version__ = '1.10.3'
 
 
 from .connectionpool import (
