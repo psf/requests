b7fc3a1250cb13ed3b55010ccc33f2681f0ffa4a
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-04-24 12:05:18 +0200

    Import aliases for Debian
    Alternative to #2375

--- requests/packages/__init__.py
+++ requests/packages/__init__.py
@@ -1,3 +1,14 @@
 from __future__ import absolute_import
+import sys
 
-from . import urllib3
+try:
+    from . import urllib3
+except ImportError:
+    import urllib3
+    sys.modules['%s.urllib3' % __name__] = urllib3
+
+try:
+    from . import chardet
+except ImportError:
+    import chardet
+    sys.modules['%s.chardet' % __name__] = chardet
