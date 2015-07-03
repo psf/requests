e9a1e35f8989691afe99407d9b65a67684aa8bac
Cory Benfield <lukasaoz@gmail.com>
2015-07-03 17:00:38 +0100

    Only pass useful timeouts to _get_conn

--- requests/adapters.py
+++ requests/adapters.py
@@ -35,6 +35,7 @@ from .auth import _basic_auth_str
 DEFAULT_POOLBLOCK = False
 DEFAULT_POOLSIZE = 10
 DEFAULT_RETRIES = 0
+DEFAULT_POOL_TIMEOUT = None
 
 
 class BaseAdapter(object):
@@ -375,7 +376,7 @@ class HTTPAdapter(BaseAdapter):
                 if hasattr(conn, 'proxy_pool'):
                     conn = conn.proxy_pool
 
-                low_conn = conn._get_conn(timeout=timeout)
+                low_conn = conn._get_conn(timeout=DEFAULT_POOL_TIMEOUT)
 
                 try:
                     low_conn.putrequest(request.method,
