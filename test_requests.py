1f48a4c9ea81d6b2128a7e1199be2dfbb81b643d
Ravi Prakash Putchala <praks.putchala>
2015-09-09 14:43:22 +0530

    Removed the other 'encode' as well, in the test case.

--- test_requests.py
+++ test_requests.py
@@ -1064,7 +1064,7 @@ class RequestsTestCase(unittest.TestCase):
 
     def test_json_param_post_should_not_override_data_param(self):
         r = requests.Request(method='POST', url='http://httpbin.org/post',
-                             data={'stuff'.encode('utf-8'): 'elixr'},
+                             data={'stuff': 'elixr'},
                              json={'music': 'flute'})
         prep = r.prepare()
         assert 'stuff=elixr' == prep.body
