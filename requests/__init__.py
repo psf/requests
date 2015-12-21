1108058626450b863d154bb74d669754b480caa4
Cory Benfield <lukasaoz@gmail.com>
2015-12-21 14:53:36 +0000

    v2.9.1

--- requests/__init__.py
+++ requests/__init__.py
@@ -42,8 +42,8 @@ is at <http://python-requests.org>.
 """
 
 __title__ = 'requests'
-__version__ = '2.9.0'
-__build__ = 0x020900
+__version__ = '2.9.1'
+__build__ = 0x020901
 __author__ = 'Kenneth Reitz'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Kenneth Reitz'
