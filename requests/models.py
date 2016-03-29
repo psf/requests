b8a577050dc4345b87afc0cd67193537fac41350
Alexander Hermes <alexander.hermes@metaswitch.com>
2016-03-29 11:57:47 +0100

    Update docstring and API doc to document ability to add per-file headers in multipart POST
    * Docstring only change - for the function itself cf. requests Issue #1640

--- requests/models.py
+++ requests/models.py
@@ -103,8 +103,10 @@ class RequestEncodingMixin(object):
         """Build the body for a multipart/form-data request.
 
         Will successfully encode files when passed as a dict or a list of
-        2-tuples. Order is retained if data is a list of 2-tuples but arbitrary
+        tuples. Order is retained if data is a list of tuples but arbitrary
         if parameters are supplied as a dict.
+        The tuples may be 2-tuples (filename, fileobj), 3-tuples (filename, fileobj, contentype)
+        or 4-tuples (filename, fileobj, contentype, custom_headers).
 
         """
         if (not files):
