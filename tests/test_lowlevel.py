35744c3e5d212f8841be3a5ab7de3e3b37dda5bb
Brett Higgins <brhiggins@arbor.net>
2016-05-16 21:53:20 -0400

    Use iter instead of noop list comprehension

--- tests/test_lowlevel.py
+++ tests/test_lowlevel.py
@@ -12,7 +12,7 @@ def test_chunked_upload():
     """can safely send generators"""
     close_server = threading.Event()
     server = Server.basic_response_server(wait_to_close_event=close_server)
-    data = (i for i in [b'a', b'b', b'c'])
+    data = iter([b'a', b'b', b'c'])
 
     with server as (host, port):
         url = 'http://{0}:{1}/'.format(host, port)
