9e9d2c6521e37e1c4f35e04f95a910ad47e28958
Seppo Yli-Olli <seppo.yli-olli@iki.fi>
2016-05-24 21:23:16 +0300

    Make BaseAdapter describe the mandatory adapter interface (#3216)
    * BaseAdapter definition of send is missing mandatory params
    
    * Copy over relevant parts of the interface documentation
    
    * Indentation fix
    
    * Change base class documentation for close

--- requests/adapters.py
+++ requests/adapters.py
@@ -54,10 +54,24 @@ class BaseAdapter(object):
     def __init__(self):
         super(BaseAdapter, self).__init__()
 
-    def send(self):
+    def send(self, request, stream=False, timeout=None, verify=True,
+             cert=None, proxies=None):
+        """Sends PreparedRequest object. Returns Response object.
+
+        :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
+        :param stream: (optional) Whether to stream the request content.
+        :param timeout: (optional) How long to wait for the server to send
+            data before giving up, as a float, or a :ref:`(connect timeout,
+            read timeout) <timeouts>` tuple.
+        :type timeout: float or tuple
+        :param verify: (optional) Whether to verify SSL certificates.
+        :param cert: (optional) Any user-provided SSL certificate to be trusted.
+        :param proxies: (optional) The proxies dictionary to apply to the request.
+        """
         raise NotImplementedError
 
     def close(self):
+        """Cleans up adapter specific items."""
         raise NotImplementedError
 
 
