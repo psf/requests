21f8ee7e2a69ddf96662090a0f781640d8725d1e
Nate Prewitt <Nate.Prewitt@gmail.com>
2016-06-24 09:17:58 -0400

    adding asserted_encoding check on None type encoding to match text() behavior

--- requests/utils.py
+++ requests/utils.py
@@ -358,13 +358,20 @@ def get_encoding_from_headers(headers):
 
 def stream_decode_response_unicode(iterator, r):
     """Stream decodes a iterator."""
+    encoding = r.encoding
 
-    if r.encoding is None:
-        for item in iterator:
-            yield item
-        return
+    if encoding is None:
+        encoding = r.apparent_encoding
 
-    decoder = codecs.getincrementaldecoder(r.encoding)(errors='replace')
+    try:
+        decoder = codecs.getincrementaldecoder(encoding)(errors='replace')
+    except (LookupError, TypeError):
+        # A LookupError is raised if the encoding was not found which could
+        # indicate a misspelling or similar mistake.
+        #
+        # A TypeError can be raised if encoding is None
+        raise UnicodeError("Unable to decode contents with encoding %s." % encoding)
+
     for chunk in iterator:
         rv = decoder.decode(chunk)
         if rv:
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -980,6 +980,13 @@ class TestRequests:
         chunks = r.iter_content(decode_unicode=True)
         assert all(isinstance(chunk, str) for chunk in chunks)
 
+        # check for encoding value of None
+        r = requests.Response()
+        r.raw = io.BytesIO(b'the content')
+        r.encoding = None
+        chunks = r.iter_content(decode_unicode=True)
+        assert all(isinstance(chunk, str) for chunk in chunks)
+
     def test_response_chunk_size_int(self):
         """Ensure that chunk_size is passed as an integer, otherwise
         raise a TypeError.
