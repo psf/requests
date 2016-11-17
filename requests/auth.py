329a5cfdd4500fb1ce405a3242ef95f9197d24ba
Dmitry Klimenko <github.com@klimenko.net>
2016-11-17 19:50:53 +0300

    Order of type check

--- requests/auth.py
+++ requests/auth.py
@@ -28,14 +28,14 @@ CONTENT_TYPE_MULTI_PART = 'multipart/form-data'
 def _basic_auth_str(username, password):
     """Returns a Basic Auth string."""
     
-    if isinstance(username, bytes):
-        username = username.decode('latin1')
+    if isinstance(username, str):
+        username = username.encode('latin1')
 
-    if isinstance(password, bytes):
-        password = password.decode('latin1')
+    if isinstance(password, str):
+        password = password.encode('latin1')
 
     authstr = 'Basic ' + to_native_string(
-        b64encode(('%s:%s' % (username, password)).encode('latin1')).strip()
+        b64encode(b':'.join((username, password))).strip()
     )
 
     return authstr
