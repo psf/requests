5aa66615870beeb7b0ed3af27457f689db262ab3
John Dingee <dingeej@gmail.com>
2016-12-21 13:08:45 -0500

    fix urllib3 documentation link (#3785)

--- requests/packages/urllib3/contrib/appengine.py
+++ requests/packages/urllib3/contrib/appengine.py
@@ -111,7 +111,7 @@ class AppEngineManager(RequestMethods):
         warnings.warn(
             "urllib3 is using URLFetch on Google App Engine sandbox instead "
             "of sockets. To use sockets directly instead of URLFetch see "
-            "https://urllib3.readthedocs.io/en/latest/contrib.html.",
+            "https://urllib3.readthedocs.io/en/latest/reference/urllib3.contrib.html.",
             AppEnginePlatformWarning)
 
         RequestMethods.__init__(self, headers)
