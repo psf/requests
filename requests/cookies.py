a881d307fc4d5feb4e2abdea65b41b381ff80d7e
Atterratio <atterratio@gmail.com>
2017-01-09 07:33:34 +0400

    Update cookies.py
    Add **kwargs to cookiejar_from_dict

--- requests/cookies.py
+++ requests/cookies.py
@@ -500,7 +500,7 @@ def morsel_to_cookie(morsel):
     )
 
 
-def cookiejar_from_dict(cookie_dict, cookiejar=None, overwrite=True):
+def cookiejar_from_dict(cookie_dict, cookiejar=None, overwrite=True, **kwargs):
     """Returns a CookieJar from a key/value dictionary.
 
     :param cookie_dict: Dict of key/values to insert into CookieJar.
@@ -515,7 +515,7 @@ def cookiejar_from_dict(cookie_dict, cookiejar=None, overwrite=True):
         names_from_jar = [cookie.name for cookie in cookiejar]
         for name in cookie_dict:
             if overwrite or (name not in names_from_jar):
-                cookiejar.set_cookie(create_cookie(name, cookie_dict[name]))
+                cookiejar.set_cookie(create_cookie(name, cookie_dict[name], **kwargs))
 
     return cookiejar
 
