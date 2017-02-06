b00d8bf474cf5e2850ba83a11b7e372dad4bf119
Marcos Dione <mdione@grulic.org.ar>
2017-02-06 21:44:28 +0100

    Proper version, no bugs, shorter.

--- requests/status_codes.py
+++ requests/status_codes.py
@@ -87,5 +87,5 @@ codes = LookupDict(name='status_codes')
 for code, titles in _codes.items():
     for title in titles:
         setattr(codes, title, code)
-        if not title.startswith('\\') and not title.strartswith('/'):
+        if not if not title.startswith(('\\', '/')):
             setattr(codes, title.upper(), code)
