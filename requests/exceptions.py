3046a1eb541f543dc4bd24f079e7908a502e765c
Nate Prewitt <Nate.Prewitt@gmail.com>
2017-02-28 16:08:48 -0700

    remove redundant exception declaration

--- requests/exceptions.py
+++ requests/exceptions.py
@@ -85,10 +85,6 @@ class InvalidHeader(RequestException, ValueError):
     """The header value provided was somehow invalid."""
 
 
-class InvalidHeader(RequestException, ValueError):
-    """The header value provided was somehow invalid."""
-
-
 class ChunkedEncodingError(RequestException):
     """The server declared chunked encoding but sent an invalid chunk."""
 
