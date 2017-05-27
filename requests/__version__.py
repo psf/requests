282b01a7c9feae30feccbd54e33afcc06197df62
Kenneth Reitz <me@kennethreitz.org>
2017-05-26 22:13:29 -0400

    new release

--- requests/__version__.py
+++ requests/__version__.py
@@ -7,8 +7,8 @@
 __title__ = 'requests'
 __description__ = 'Python HTTP for Humans.'
 __url__ = 'http://python-requests.org'
-__version__ = '2.15.0'
-__build__ = 0x021500
+__version__ = '2.15.1'
+__build__ = 0x021501
 __author__ = 'Kenneth Reitz'
 __author_email__ = 'me@kennethreitz.org'
 __license__ = 'Apache 2.0'
