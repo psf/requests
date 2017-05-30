98bbf8d17088cc9e11b30038e451cbafba1ef7e2
Jacob Mansfield <cyberjacob@gmail.com>
2017-05-30 11:01:34 +0100

    Catch error if multiprocessing is not available or is unable to determine the number of CPUs

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ from codecs import open
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
-from multiprocessing import cpu_count
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,7 +16,11 @@ class PyTest(TestCommand):
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = ['-n', str(cpu_count()), '--boxed']
+        try:
+            from multiprocessing import cpu_count
+            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
+        except (ImportError, NotImplementedError):
+            self.pytest_args = ['-n', '1', '--boxed']
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
