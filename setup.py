fbe5a7a076e67bed281bf8750cbafe8c2c0611b4
mfranke <mf@hlinux.net>
2017-05-30 17:17:43 +0200

    update extra require with limitations for cchardet

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setup(
     cmdclass={'test': PyTest},
     tests_require=test_requirements,
     extras_require={
+        'cchardet:(python_version == "2.7" or python_version >= "3.4")': ['cchardet>=2.1.0'],
         'security': ['pyOpenSSL>=0.14', 'cryptography>=1.3.4', 'idna>=2.0.0'],
         'socks': ['PySocks>=1.5.6, !=1.5.7'],
         'socks:sys_platform == "win32" and (python_version == "2.7" or python_version == "2.6")': ['win_inet_pton'],
