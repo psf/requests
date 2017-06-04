89ab030cdb83a728a30e172bc65d27ba214d2eda
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-06-04 14:12:08 -0300

    Use comprehensions whenever possible

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -665,7 +665,7 @@ def test_add_dict_to_cookiejar(cookiejar):
     cookiedict = {'test': 'cookies',
                   'good': 'cookies'}
     cj = add_dict_to_cookiejar(cookiejar, cookiedict)
-    cookies = dict((cookie.name, cookie.value) for cookie in cj)
+    cookies = {cookie.name: cookie.value for cookie in cj}
     assert cookiedict == cookies
 
 
