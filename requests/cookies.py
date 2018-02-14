910c9528dcaed290efbfec9a6e2350cb344f54c9
Laurent Bachelier <laurent@bachelier.name>
2018-02-14 15:26:13 +0100

    Copy cookie policy when copying a CookieJar
    Without it, the cookie policy would get lost while making a request.
    That would be invisible to the user if the default cookie policy wasn't
    changed, of course.

--- requests/cookies.py
+++ requests/cookies.py
@@ -415,6 +415,7 @@ class RequestsCookieJar(cookielib.CookieJar, collections.MutableMapping):
     def copy(self):
         """Return a copy of this RequestsCookieJar."""
         new_cj = RequestsCookieJar()
+        new_cj.set_policy(self._policy)
         new_cj.update(self)
         return new_cj
 
