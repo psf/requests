d956dc1d7050aa42bb5a1d070f41b613daab9290
Kenneth Reitz <me@kennethreitz.org>
2018-03-15 16:53:58 -0400

    no more session
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- requests/__init__.py
+++ requests/__init__.py
@@ -116,7 +116,7 @@ from .__version__ import __copyright__, __cake__
 from .import utils
 from .models import Request, Response, PreparedRequest
 from .api import request, get, head, post, patch, put, delete, options
-from .sessions import session, Session
+from .sessions import Session
 from .status_codes import codes
 from .exceptions import (
     RequestException,
