8f3f022c54db7c8348c84c079dfe7f7c354f1062
Kenneth Reitz <me@kennethreitz.org>
2018-03-15 17:25:00 -0400

    attempt to fix appveyor
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -644,10 +644,8 @@ def test_should_bypass_proxies_win_registry(
     """
     if override is None:
         override = '192.168.*;127.0.0.1;localhost.localdomain;172.16.1.1'
-    if basics.is_py3:
-        import winreg
-    else:
-        import _winreg as winreg
+
+    import winreg
 
     class RegHandle:
 
