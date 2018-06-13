1c13c5e6f35cf05c4de758714bc99919e9bbbb43
Steven M. Vascellaro <svascellaro@gmail.com>
2018-06-13 12:43:05 -0400

    Document that `requests.session()` is depreciated
    Adds a docstring note that `requests.session()` is depreciated. Users should
    use `requests.Session()` in versions released after Requests v1.0.

--- requests/sessions.py
+++ requests/sessions.py
@@ -735,7 +735,12 @@ def session():
     """
     Returns a :class:`Session` for context-management.
 
+    .. deprecated:: 1.0.0
+
+        This method has been deprecated since version 1.0.0 and is only kept for
+        backwards compatibility. New code should use :class:`~requests.sessions.Session`
+        to create a session. This may be removed at a future date.
+
     :rtype: Session
     """
-
     return Session()
