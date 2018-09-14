09139dcda5d2acf3df9cb048f290e0e073206e2e
Roger Collins <zulrang@gmail.com>
2018-09-14 10:35:40 -0400

    Allow headers to be passed to proxy
    Adding a `header` field to `proxies` will pass any headers to the proxy connection.

--- requests/adapters.py
+++ requests/adapters.py
@@ -306,6 +306,7 @@ class HTTPAdapter(BaseAdapter):
                 raise InvalidProxyURL("Please check proxy URL. It is malformed"
                                       " and could be missing the host.")
             proxy_manager = self.proxy_manager_for(proxy)
+            proxy_manager.proxy_headers.update(proxies.get('headers', {}))
             conn = proxy_manager.connection_from_url(url)
         else:
             # Only scheme should be lower case
