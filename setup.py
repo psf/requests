bd840450c0d1e9db3bf62382c15d96378cc3a056
Nate Prewitt <Nate.Prewitt@gmail.com>
2018-10-16 13:06:29 -0700

    v2.20.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ packages = ['requests']
 requires = [
     'chardet>=3.0.2,<3.1.0',
     'idna>=2.5,<2.8',
-    'urllib3>=1.21.1,<1.24',
+    'urllib3>=1.21.1,<1.25',
     'certifi>=2017.4.17'
 
 ]
