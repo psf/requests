ffee24da8d459da5be62343048767ca19c13c89d
Alexander Overvoorde <overv161@gmail.com>
2019-10-01 21:55:27 +0200

    Change requests.ConnectionError to inherit standard ConnectionError (fixes #5162)

--- requests/exceptions.py
+++ requests/exceptions.py
@@ -29,7 +29,7 @@ class HTTPError(RequestException):
     """An HTTP error occurred."""
 
 
-class ConnectionError(RequestException):
+class ConnectionError(RequestException, ConnectionError):
     """A Connection error occurred."""
 
 
