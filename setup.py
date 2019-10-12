51cb0fb45b6f8af5f24557f0c1390f9b8cfda6b7
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-10-13 01:45:56 +0530

    improved setup.py
    Added upload command, that improves the setup.py publish support.

--- setup.py
+++ setup.py
@@ -33,11 +33,41 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
-# 'setup.py publish' shortcut.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    sys.exit()
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
 
 packages = ['requests']
 
@@ -109,5 +139,9 @@ setup(
     project_urls={
         'Documentation': 'http://docs.python-requests.org',
         'Source': 'https://github.com/psf/requests',
+    },
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
     },
 )
