8e59e9838004af18841d7ae42d3893437f3a48b5
Shiva Reddy <kotishiva@gmail.com>
2020-01-28 14:43:46 +0530

    updated spaces

--- requests/models.py
+++ requests/models.py
@@ -37,7 +37,7 @@ from .utils import (
     iter_slices, guess_json_utf, super_len, check_header_validity)
 from .compat import (
     Callable, Mapping,
-    cookielib, urlunparse, urlsplit, urlencode,quote,str, bytes,
+    cookielib, urlunparse, urlsplit, urlencode, quote, str, bytes,
     is_py2, chardet, builtin_str, basestring)
 from .compat import json as complexjson
 from .status_codes import codes
