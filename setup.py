f9dc5ca51263c2993e5f84099a9d9899744dbaec
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-02-18 10:57:18 +0000

    Bump maximum version of idna

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ packages = ['requests']
 
 requires = [
     'chardet>=3.0.2,<3.1.0',
-    'idna>=2.5,<2.9',
+    'idna>=2.5,<2.10',
     'urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1',
     'certifi>=2017.4.17'
 
