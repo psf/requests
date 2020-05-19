9bbc3ef9693c72fc641de09d91d422eb337405de
Sawood Alam <ibnesayeed@gmail.com>
2020-05-19 19:51:15 -0400

    Add unit test to cover semicolon in URLs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -557,6 +557,10 @@ def test_iter_slices(value, length):
             [{'url': 'http:/.../front.jpeg', 'rel': 'front', 'type': 'image/jpeg'}]
         ),
         (
+            '<https:/.../front.jpeg?a=b;id=123>; rel="original"',
+            [{'url': 'http:/.../front.jpeg?a=b;id=123', 'rel': 'original'}]
+        ),
+        (
             '<http:/.../front.jpeg>',
             [{'url': 'http:/.../front.jpeg'}]
         ),
