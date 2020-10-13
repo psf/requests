d281f4205250a402302b86dd720d325a3fc46648
hughesp8 <hughesp1997@gmail.com>
2020-10-13 16:44:45 +0100

    Added python 3.9 to CI, tox and setup.py

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
