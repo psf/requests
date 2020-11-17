1137ceadd2abcf6df625018c738bac4c32b93b1d
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-11-17 23:46:43 +0200

    Remove the deprecated security extra

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
     cmdclass={'test': PyTest},
     tests_require=test_requirements,
     extras_require={
-        'security': ['pyOpenSSL >= 0.14', 'cryptography>=1.3.4'],
         'socks': ['PySocks>=1.5.6, !=1.5.7'],
         'socks:sys_platform == "win32" and python_version == "2.7"': ['win_inet_pton'],
     },
