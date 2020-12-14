6de4f104cd8813e3fb2fca6c8297211794728cad
Shouro <sourogts@gmail.com>
2020-12-15 05:49:49 +0600

    Change docs title to requests.sessions

--- requests/sessions.py
+++ requests/sessions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-requests.session
-~~~~~~~~~~~~~~~~
+requests.sessions
+~~~~~~~~~~~~~~~~~
 
 This module provides a Session object to manage and persist settings across
 requests (cookies, auth, proxies).
