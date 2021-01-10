03c7239f90cdc5c7296aa3b46ff6ca6f2985d5d8
Sam Bragg <sam.bragg@gmail.com>
2021-01-10 16:24:40 +0100

    Make raise_for_status() chainable

--- requests/models.py
+++ requests/models.py
@@ -941,6 +941,8 @@ class Response(object):
 
         if http_error_msg:
             raise HTTPError(http_error_msg, response=self)
+            
+        return self
 
     def close(self):
         """Releases the connection back to the pool. Once this method has been
