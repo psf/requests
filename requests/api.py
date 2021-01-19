44a7a0351dee01c59bab2e9e56c9142db77bf076
Shreyan Avigyan <74560907+shreyanavigyan@users.noreply.github.com>
2021-01-20 01:38:39 +0530

    To work just like normal browsers, default headers in  requests.api.get(), requests.api.request(), requests.models.Request.__init__() and requests.sessions.Session.request() are set
    The headers argument in `requests.api.get()`, `requests.api.request()`, `requests.models.Request.__init__()` and `requests.sessions.Session.request()` are set to a default dictionary value rather than NoneType Data Type to handle errors like 503 server error though the URL is correct. Some websites block webscrapers. To avoid being blocked, this default headers is very useful. This works as of January 2021.

--- requests/api.py
+++ requests/api.py
@@ -57,10 +57,28 @@ def request(method, url, **kwargs):
     # Use the default headers
     try:
         if headers is None:
-            headers = {
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            if sys.platform.lower() == 'win32':
+                headers = {
+                    'User-Agent':
+                    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+                    }
+            elif sys.platform.lower() == 'linux':
+                headers = {
+                    'User-Agent':
+                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                 'Accept-Language': 'en-US, en;q=0.5'
+                 }
+            elif sys.platform.lower() == 'darwin':
+                headers = {
+                    'User-Agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8'
                 }
+            else:
+                headers = {
+                    'User-Agent':
+                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                 'Accept-Language': 'en-US, en;q=0.5'
+                 }
     except NameError:
         pass
 
@@ -88,10 +106,28 @@ def get(url, params=None, **kwargs):
     # Use the default headers
     try:
         if headers is None:
-            headers = {
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            if sys.platform.lower() == 'win32':
+                headers = {
+                    'User-Agent':
+                    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+                    }
+            elif sys.platform.lower() == 'linux':
+                headers = {
+                    'User-Agent':
+                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                 'Accept-Language': 'en-US, en;q=0.5'
+                 }
+            elif sys.platform.lower() == 'darwin':
+                headers = {
+                    'User-Agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8'
                 }
+            else:
+                headers = {
+                    'User-Agent':
+                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                 'Accept-Language': 'en-US, en;q=0.5'
+                 }
     except NameError:
         pass
 
