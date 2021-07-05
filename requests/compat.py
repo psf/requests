482578e856cc22cce1017cd92225a2da996b5508
Steve Berdy <86739818+steveberdy@users.noreply.github.com>
2021-07-05 12:29:54 -0400

    Update compat.py

--- requests/compat.py
+++ requests/compat.py
@@ -8,7 +8,6 @@ This module handles import compatibility issues between Python 2 and
 Python 3.
 """
 
-import json
 import chardet
 
 import sys
@@ -25,6 +24,11 @@ is_py2 = (_ver[0] == 2)
 
 #: Python 3.x?
 is_py3 = (_ver[0] == 3)
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 
 # ---------
