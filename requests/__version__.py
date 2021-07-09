a1a6a549a0143d9b32717dbe3d75cd543ae5a4f6
Nate Prewitt <Nate.Prewitt@gmail.com>
2021-07-09 12:12:59 -0700

    v2.26.0

--- requests/__version__.py
+++ requests/__version__.py
@@ -5,8 +5,8 @@
 __title__ = 'requests'
 __description__ = 'Python HTTP for Humans.'
 __url__ = 'https://requests.readthedocs.io'
-__version__ = '2.25.1'
-__build__ = 0x022501
+__version__ = '2.26.0'
+__build__ = 0x022600
 __author__ = 'Kenneth Reitz'
 __author_email__ = 'me@kennethreitz.org'
 __license__ = 'Apache 2.0'
