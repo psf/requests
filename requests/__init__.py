3cf04eb52248c0ee686ce1e597755482fbc82f77
Olawale Oyeyipo <olaclassik@gmail.com>
2022-04-11 20:22:41 +0100

    rewrite version check and split, use iteration protocol

--- requests/__init__.py
+++ requests/__init__.py
@@ -63,22 +63,17 @@ def check_compatibility(urllib3_version, chardet_version, charset_normalizer_ver
         urllib3_version.append('0')
 
     # Check urllib3 for compatibility.
-    major, minor, patch = urllib3_version  # noqa: F811
-    major, minor, patch = int(major), int(minor), int(patch)
+    major, minor, patch = map(int, urllib3_version)  # noqa: F811
     # urllib3 >= 1.21.1, <= 1.26
-    assert major == 1
-    assert minor >= 21
-    assert minor <= 26
+    assert (1, 21, 1) <= (major, minor, patch) <= (1, 26)
 
     # Check charset_normalizer for compatibility.
     if chardet_version:
-        major, minor, patch = chardet_version.split('.')[:3]
-        major, minor, patch = int(major), int(minor), int(patch)
+        major, minor, patch = map(int, chardet_version.split('.')[:3])
         # chardet_version >= 3.0.2, < 5.0.0
         assert (3, 0, 2) <= (major, minor, patch) < (5, 0, 0)
     elif charset_normalizer_version:
-        major, minor, patch = charset_normalizer_version.split('.')[:3]
-        major, minor, patch = int(major), int(minor), int(patch)
+        major, minor, patch = map(int, charset_normalizer_version.split('.')[:3])
         # charset_normalizer >= 2.0.0 < 3.0.0
         assert (2, 0, 0) <= (major, minor, patch) < (3, 0, 0)
     else:
