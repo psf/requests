9bb6eb48543d7b7138e582f09b1e7a9a4cf3c489
purajit <malalur2@illinois.edu>
2022-08-31 15:42:38 -0400

    fix order of verify overrides in Sessions

--- requests/sessions.py
+++ requests/sessions.py
@@ -760,20 +760,20 @@ class Session(SessionRedirectMixin):
             for (k, v) in env_proxies.items():
                 proxies.setdefault(k, v)
 
-            # Look for requests environment configuration
-            # and be compatible with cURL.
-            if verify is True or verify is None:
-                verify = (
-                    os.environ.get("REQUESTS_CA_BUNDLE")
-                    or os.environ.get("CURL_CA_BUNDLE")
-                    or verify
-                )
-
         # Merge all the kwargs.
         proxies = merge_setting(proxies, self.proxies)
         stream = merge_setting(stream, self.stream)
-        verify = merge_setting(verify, self.verify)
         cert = merge_setting(cert, self.cert)
+        verify = merge_setting(verify, self.verify)
+
+        # Look for requests environment configuration
+        # and be compatible with cURL.
+        if verify is True or verify is None:
+            verify = (
+                os.environ.get("REQUESTS_CA_BUNDLE")
+                or os.environ.get("CURL_CA_BUNDLE")
+                or verify
+            )
 
         return {"proxies": proxies, "stream": stream, "verify": verify, "cert": cert}
 
