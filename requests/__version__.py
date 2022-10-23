53a0562331c4a825036a347e77984d68b310fcf3
Volker Schaus <volker.schaus@esa.int>
2022-10-23 18:35:43 +0000

    change to SPDX conform license string

--- requests/__version__.py
+++ requests/__version__.py
@@ -9,6 +9,6 @@ __version__ = "2.28.1"
 __build__ = 0x022801
 __author__ = "Kenneth Reitz"
 __author_email__ = "me@kennethreitz.org"
-__license__ = "Apache 2.0"
+__license__ = "Apache-2.0"
 __copyright__ = "Copyright 2022 Kenneth Reitz"
 __cake__ = "\u2728 \U0001f370 \u2728"
