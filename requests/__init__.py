5ad83acbb5cfd5c3c9c6e05daee68e1d0a52aaf8
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2022-11-08 20:08:29 +0530

    Update __init__.py
    replaced try with suppress and little bit rewrite code

--- requests/__init__.py
+++ requests/__init__.py
@@ -38,6 +38,39 @@ is at <https://requests.readthedocs.io>.
 :license: Apache 2.0, see LICENSE for more details.
 """
 
+import contextlib
+from .status_codes import codes
+from .sessions import Session, session
+from .models import PreparedRequest, Request, Response
+from .exceptions import (
+    ConnectionError,
+    ConnectTimeout,
+    FileModeWarning,
+    HTTPError,
+    JSONDecodeError,
+    ReadTimeout,
+    RequestException,
+    Timeout,
+    TooManyRedirects,
+    URLRequired,
+)
+from .api import delete, get, head, options, patch, post, put, request
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __build__,
+    __cake__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+from . import packages, utils
+from logging import NullHandler
+import logging
+from urllib3.exceptions import DependencyWarning
 import warnings
 
 import urllib3
@@ -57,7 +90,8 @@ except ImportError:
 
 def check_compatibility(urllib3_version, chardet_version, charset_normalizer_version):
     urllib3_version = urllib3_version.split(".")
-    assert urllib3_version != ["dev"]  # Verify urllib3 isn't installed from git.
+    # Verify urllib3 isn't installed from git.
+    assert urllib3_version != ["dev"]
 
     # Sometimes, urllib3 only reports its version as 16.1.
     if len(urllib3_version) == 2:
@@ -80,10 +114,11 @@ def check_compatibility(urllib3_version, chardet_version, charset_normalizer_ver
     elif charset_normalizer_version:
         major, minor, patch = charset_normalizer_version.split(".")[:3]
         major, minor, patch = int(major), int(minor), int(patch)
-        # charset_normalizer >= 2.0.0 < 4.0.0
-        assert (2, 0, 0) <= (major, minor, patch) < (4, 0, 0)
+        # charset_normalizer >= 2.0.0 < 3.0.0
+        assert (2, 0, 0) <= (major, minor, patch) < (3, 0, 0)
     else:
-        raise Exception("You need either charset_normalizer or chardet installed")
+        raise Exception(
+            "You need either charset_normalizer or chardet installed")
 
 
 def _check_cryptography(cryptography_version):
@@ -94,9 +129,8 @@ def _check_cryptography(cryptography_version):
         return
 
     if cryptography_version < [1, 3, 4]:
-        warning = "Old version of cryptography ({}) may cause slowdown.".format(
-            cryptography_version
-        )
+        warning = f"Old version of cryptography ({cryptography_version}) may cause slowdown."
+
         warnings.warn(warning, RequestsDependencyWarning)
 
 
@@ -107,17 +141,13 @@ try:
     )
 except (AssertionError, ValueError):
     warnings.warn(
-        "urllib3 ({}) or chardet ({})/charset_normalizer ({}) doesn't match a supported "
-        "version!".format(
-            urllib3.__version__, chardet_version, charset_normalizer_version
-        ),
-        RequestsDependencyWarning,
-    )
+        f"urllib3 ({urllib3.__version__}) or chardet ({chardet_version})/charset_normalizer ({charset_normalizer_version}) doesn't match a supported version!", RequestsDependencyWarning)
+
 
 # Attempt to enable urllib3's fallback for SNI support
 # if the standard library doesn't support SNI or the
 # 'ssl' library isn't available.
-try:
+with contextlib.suppress(ImportError):
     try:
         import ssl
     except ImportError:
@@ -132,47 +162,12 @@ try:
         from cryptography import __version__ as cryptography_version
 
         _check_cryptography(cryptography_version)
-except ImportError:
-    pass
-
 # urllib3's DependencyWarnings should be silenced.
-from urllib3.exceptions import DependencyWarning
 
 warnings.simplefilter("ignore", DependencyWarning)
 
 # Set default logging handler to avoid "No handler found" warnings.
-import logging
-from logging import NullHandler
 
-from . import packages, utils
-from .__version__ import (
-    __author__,
-    __author_email__,
-    __build__,
-    __cake__,
-    __copyright__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-)
-from .api import delete, get, head, options, patch, post, put, request
-from .exceptions import (
-    ConnectionError,
-    ConnectTimeout,
-    FileModeWarning,
-    HTTPError,
-    JSONDecodeError,
-    ReadTimeout,
-    RequestException,
-    Timeout,
-    TooManyRedirects,
-    URLRequired,
-)
-from .models import PreparedRequest, Request, Response
-from .sessions import Session, session
-from .status_codes import codes
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
