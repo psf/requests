358f5026fc504d2181ff8c2098a3c2de074de332
RitvikY <ritviky@terpmail.umd.edu>
2023-05-08 15:34:54 -0400

    optimizing the resolve-redirects method in sessions.py

--- requests/sessions.py
+++ requests/sessions.py
@@ -171,6 +171,8 @@ class SessionRedirectMixin:
         """Receives a Response. Returns a generator of Responses or Requests."""
 
         hist = []  # keep track of history
+        purged_headers = ("Content-Length", "Content-Type", "Transfer-Encoding")
+        temporary_and_permanent_redirect = (codes.temporary_redirect, codes.permanent_redirect)
 
         url = self.get_redirect_target(resp)
         previous_fragment = urlparse(req.url).fragment
@@ -194,63 +196,22 @@ class SessionRedirectMixin:
 
             # Release the connection back into the pool.
             resp.close()
-
-            # Handle redirection without scheme (see: RFC 1808 Section 4)
-            if url.startswith("//"):
-                parsed_rurl = urlparse(resp.url)
-                url = ":".join([to_native_string(parsed_rurl.scheme), url])
-
-            # Normalize url case and attach previous fragment if needed (RFC 7231 7.1.2)
-            parsed = urlparse(url)
-            if parsed.fragment == "" and previous_fragment:
-                parsed = parsed._replace(fragment=previous_fragment)
-            elif parsed.fragment:
-                previous_fragment = parsed.fragment
-            url = parsed.geturl()
-
-            # Facilitate relative 'location' headers, as allowed by RFC 7231.
-            # (e.g. '/path/to/resource' instead of 'http://domain.tld/path/to/resource')
-            # Compliant with RFC3986, we percent encode the url.
-            if not parsed.netloc:
-                url = urljoin(resp.url, requote_uri(url))
-            else:
-                url = requote_uri(url)
+            url = self._resolve_redirect_url(url, resp, previous_fragment)
 
             prepared_request.url = to_native_string(url)
 
             self.rebuild_method(prepared_request, resp)
 
-            # https://github.com/psf/requests/issues/1084
-            if resp.status_code not in (
-                codes.temporary_redirect,
-                codes.permanent_redirect,
-            ):
-                # https://github.com/psf/requests/issues/3490
-                purged_headers = ("Content-Length", "Content-Type", "Transfer-Encoding")
+            if resp.status_code not in temporary_and_permanent_redirect:
                 for header in purged_headers:
                     prepared_request.headers.pop(header, None)
                 prepared_request.body = None
 
-            headers = prepared_request.headers
-            headers.pop("Cookie", None)
+            prepared_request.headers.pop("Cookie", None)
 
-            # Extract any cookies sent on the response to the cookiejar
-            # in the new request. Because we've mutated our copied prepared
-            # request, use the old one that we haven't yet touched.
-            extract_cookies_to_jar(prepared_request._cookies, req, resp.raw)
-            merge_cookies(prepared_request._cookies, self.cookies)
-            prepared_request.prepare_cookies(prepared_request._cookies)
+            self._prepare_cookies_and_proxies(prepared_request, req, resp, proxies)
 
-            # Rebuild auth and proxy information.
-            proxies = self.rebuild_proxies(prepared_request, proxies)
-            self.rebuild_auth(prepared_request, resp)
-
-            # A failed tell() sets `_body_position` to `object()`. This non-None
-            # value ensures `rewindable` will be True, allowing us to raise an
-            # UnrewindableBodyError, instead of hanging the connection.
-            rewindable = prepared_request._body_position is not None and (
-                "Content-Length" in headers or "Transfer-Encoding" in headers
-            )
+            rewindable = self._check_if_rewindable(prepared_request, headers)
 
             # Attempt to rewind consumed file-like object.
             if rewindable:
@@ -262,23 +223,78 @@ class SessionRedirectMixin:
             if yield_requests:
                 yield req
             else:
-
-                resp = self.send(
-                    req,
-                    stream=stream,
-                    timeout=timeout,
-                    verify=verify,
-                    cert=cert,
-                    proxies=proxies,
-                    allow_redirects=False,
-                    **adapter_kwargs,
+                resp = self._send_and_extract_cookies(
+                    prepared_request,
+                    stream,
+                    timeout,
+                    verify,
+                    cert,
+                    proxies,
+                    adapter_kwargs,
                 )
 
-                extract_cookies_to_jar(self.cookies, prepared_request, resp.raw)
-
-                # extract redirect url, if any, for the next loop
+                # Extract redirect url, if any, for the next loop
                 url = self.get_redirect_target(resp)
                 yield resp
+
+    def _resolve_redirect_url(self, url, resp, previous_fragment):
+        if url.startswith("//"):
+            parsed_rurl = urlparse(resp.url)
+            url = ":".join([to_native_string(parsed_rurl.scheme), url])
+
+        parsed = urlparse(url)
+        if not parsed.fragment:
+            parsed = parsed._replace(fragment=previous_fragment)
+        elif parsed.fragment:
+            previous_fragment = parsed.fragment
+        url = parsed.geturl()
+
+        if not parsed.netloc:
+            url = urljoin(resp.url, requote_uri(url))
+        else:
+            url = requote_uri(url)
+
+        return url
+
+    def _prepare_cookies_and_proxies(self, prepared_request, req, resp, proxies):
+        extract_cookies_to_jar(prepared_request._cookies, req, resp.raw)
+        merge_cookies(prepared_request._cookies, self.cookies)
+        prepared_request.prepare_cookies(prepared_request._cookies)
+
+        proxies = self.rebuild_proxies(prepared_request, proxies)
+        self.rebuild_auth(prepared_request, resp)
+
+    def _check_if_rewindable(self, prepared_request, headers):
+        return (
+            prepared_request._body_position is not None
+            and (
+                "Content-Length" in headers or "Transfer-Encoding" in headers
+            )
+        )
+    
+    def _send_and_extract_cookies(
+            self,
+            prepared_request,
+            stream,
+            timeout,
+            verify,
+            cert,
+            proxies,
+            adapter_kwargs,
+        ):
+        resp = self.send(
+            prepared_request,
+            stream=stream,
+            timeout=timeout,
+            verify=verify,
+            cert=cert,
+            proxies=proxies,
+            allow_redirects=False,
+            **adapter_kwargs,
+        )
+
+        extract_cookies_to_jar(self.cookies, prepared_request, resp.raw)
+        return resp
 
     def rebuild_auth(self, prepared_request, response):
         """When being redirected we may want to strip authentication from the
