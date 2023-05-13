b639e66c816514e40604d46f0088fbceec1a5149
Thomas Grainger <tagrain@gmail.com>
2023-05-13 15:10:56 +0100

    test on py3.12 (#6448)
    * test on py3.12
    
    * update to pytest-httpbin==2.0.0

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ requires = [
     "certifi>=2017.4.17",
 ]
 test_requirements = [
-    "pytest-httpbin==0.0.7",
+    "pytest-httpbin==2.0.0",
     "pytest-cov",
     "pytest-mock",
     "pytest-xdist",
