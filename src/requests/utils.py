c32b046243dcbf0dceb952da1317261109ac45c4
Matthew Carruth <carruthm@gmail.com>
2023-11-15 17:00:28 -0800

    Fix missing space in error message

--- src/requests/utils.py
+++ src/requests/utils.py
@@ -1051,7 +1051,7 @@ def _validate_header_part(header, header_part, header_validator_index):
     if not validator.match(header_part):
         header_kind = "name" if header_validator_index == 0 else "value"
         raise InvalidHeader(
-            f"Invalid leading whitespace, reserved character(s), or return"
+            f"Invalid leading whitespace, reserved character(s), or return "
             f"character(s) in header {header_kind}: {header_part!r}"
         )
 
