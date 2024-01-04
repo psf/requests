d41fec3e597aa879ed3ac50d68fcb6e036d01dc8
tanrbobanr <87777314+tanrbobanr@users.noreply.github.com>
2024-01-03 23:12:07 -0800

    Closes #6565; Add documentation to `certs.where`.

--- src/requests/certs.py
+++ src/requests/certs.py
@@ -11,7 +11,22 @@ If you are packaging Requests, e.g., for a Linux distribution or a managed
 environment, you can change the definition of where() to return a separately
 packaged CA bundle.
 """
-from certifi import where
+from certifi import where as _where
+
+
+def where() -> str:
+    """Returns the path of the default CA-certs bundle that is included with
+    the requests package. This function is essentially an alias of
+    `certify.where`.
+
+    Note that the default path is not necessarily the one being used, as it can
+    be overridden through ``requests.Session().verify``,
+    ``requests.Session().merge_environment_settings``,
+    ``requests.get(verify=...)``, and others.
+
+    """
+    return _where()
+
 
 if __name__ == "__main__":
-    print(where())
+    print(_where())
