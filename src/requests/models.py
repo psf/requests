3daa79dd62e2e7a97edcb973748296e089c3d09f
Kyeonghoon Kim <53330796+Xiroo@users.noreply.github.com>
2024-02-28 11:11:06 +0900

    lint
    lint

--- src/requests/models.py
+++ src/requests/models.py
@@ -1006,14 +1006,10 @@ class Response:
             reason = self.reason
 
         if 400 <= self.status_code < 500:
-            http_error_msg = (
-                f"{self.status_code} Client Error: {reason} for url: {self.url} content: {self.content}"
-            )
+            http_error_msg = f"{self.status_code} Client Error: {reason} for url: {self.url} content: {self.content}"
 
         elif 500 <= self.status_code < 600:
-            http_error_msg = (
-                f"{self.status_code} Server Error: {reason} for url: {self.url} content: {self.content}"
-            )
+            http_error_msg = f"{self.status_code} Server Error: {reason} for url: {self.url} content: {self.content}"
 
         if http_error_msg:
             raise HTTPError(http_error_msg, response=self)
