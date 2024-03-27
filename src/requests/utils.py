a3d8fe734e059177d3b831488e20f20446a3f482
Vítor Capparelli <vcapparelli@riseup.net>
2024-03-27 03:37:48 -0300

    Fix utils.is_ipv4_address

--- src/requests/utils.py
+++ src/requests/utils.py
@@ -702,8 +702,8 @@ def is_ipv4_address(string_ip):
     :rtype: bool
     """
     try:
-        socket.inet_aton(string_ip)
-    except OSError:
+        ipaddress.IPv4Address(string_ip)
+    except ipaddress.AddressValueError:
         return False
     return True
 
