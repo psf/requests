209be6032eb50c87f5d1270d7e938c633362e57e
Michal Čihař <michal@cihar.com>
2024-05-22 09:47:08 +0200

    Prefer charset_normalizer for char detection
    charset_normalizer is dependency for requests, while chardet is not, so it is more likely to be found.

--- src/requests/compat.py
+++ src/requests/compat.py
@@ -18,7 +18,7 @@ import sys
 def _resolve_char_detection():
     """Find supported character detection libraries."""
     chardet = None
-    for lib in ("chardet", "charset_normalizer"):
+    for lib in ("charset_normalizer", "chardet"):
         if chardet is None:
             try:
                 chardet = importlib.import_module(lib)
