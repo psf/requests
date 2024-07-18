4e383642a9ebb82a67eaeed199d0fbbb31991e3c
Nate Prewitt <nate.prewitt@gmail.com>
2024-07-18 09:43:49 -0700

    Add conditional string encoding based on urllib3 major version

--- src/requests/compat.py
+++ src/requests/compat.py
@@ -10,6 +10,18 @@ compatibility until the next major version.
 import importlib
 import sys
 
+# -------
+# urllib3
+# -------
+from urllib3 import __version__ as urllib3_version
+
+# Detect which major version of urllib3 is being used.
+try:
+    is_urllib3_2 = int(urllib3_version.split(".")[0]) == 2
+except (TypeError, AttributeError):
+    # If we can't discern a version, prefer old functionality.
+    is_urllib3_2 = False
+
 # -------------------
 # Character Detection
 # -------------------
