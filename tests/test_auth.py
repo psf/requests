4de3b73f99afe8a0d3c2da34db52c9ab045b4236
kalingth <wallace.faria@kalingth.com.br>
2024-07-26 00:25:32 -0300

    add(test): unity test for digestauth encoding

---
+++ tests/test_auth.py
@@ -0,0 +1,21 @@
+import pytest
+from requests.auth import HTTPDigestAuth 
+
+
+class TestDigestAuth:
+
+    def _build_a_digest_auth(self, user, password):
+        auth = HTTPDigestAuth(user, password)
+        auth.init_per_thread_state()
+        auth._thread_local.chal["realm"] = 'eggs'
+        auth._thread_local.chal["nonce"] = 'chips'
+        return auth.build_digest_header('GET', 'https://www.example.com/')
+
+    @pytest.mark.parametrize("username, password", [("spam", "ham")])
+    def test_digestauth_encode_consistency(self, username, password):
+        auth = username, password
+        str_auth = self._build_a_digest_auth(*auth)
+        bauth = username.encode('utf-8'), password.encode('utf-8')
+        bin_auth = self._build_a_digest_auth(*bauth)
+        assert str(str_auth) == str(bin_auth)
+
