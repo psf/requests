f519bbae3346b4bf0ff866e0659b7efbcf016eb1
Thomas Grainger <tagrain@gmail.com>
2024-09-18 19:14:19 +0100

    add trove classifier for 3.13

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
