fe66d15962832e0ba50f592a6f30116b992fcba2
MayureshMore <m_more1@u.pacific.edu>
2025-02-14 18:37:08 -0800

    [Edited] Add docstring to improve documentation

--- requests/auth.py
+++ requests/auth.py
@@ -1,95 +1,74 @@
-# -*- coding: utf-8 -*-
 """
 requests.auth
 ~~~~~~~~~~~~~
 
 This module contains the authentication handlers for Requests.
 """
-
 import os
 import re
 import time
 import hashlib
 import threading
-
 from base64 import b64encode
-
 from .basics import urlparse, str, basestring
 from .cookies import extract_cookies_to_jar
 from ._internal_utils import to_native_string
 from .utils import parse_dict_header
-
 CONTENT_TYPE_FORM_URLENCODED = 'application/x-www-form-urlencoded'
 CONTENT_TYPE_MULTI_PART = 'multipart/form-data'
-
 
 def _basic_auth_str(username, password):
     """Returns a Basic Auth string."""
     if not isinstance(username, basestring):
-        raise TypeError(
-            'username must be of type str or bytes, '
-            'instead it was %s' % type(username)
-        )
-
+        raise TypeError('username must be of type str or bytes, instead it was %s' % type(username))
     if not isinstance(password, basestring):
-        raise TypeError(
-            'password must be of type str or bytes, '
-            'instead it was %s' % type(password)
-        )
-
+        raise TypeError('password must be of type str or bytes, instead it was %s' % type(password))
     if isinstance(username, str):
         username = username.encode('latin1')
     if isinstance(password, str):
         password = password.encode('latin1')
-    authstr = 'Basic ' + to_native_string(
-        b64encode(b':'.join((username, password))).strip()
-    )
+    authstr = 'Basic ' + to_native_string(b64encode(b':'.join((username, password))).strip())
     return authstr
-
 
 class AuthBase(object):
     """Base class that all auth implementations derive from"""
 
     def __call__(self, r):
+        """Auto-generated docstring for function __call__."""
         raise NotImplementedError('Auth hooks must be callable.')
-
 
 class HTTPBasicAuth(AuthBase):
     """Attaches HTTP Basic Authentication to the given Request object."""
 
     def __init__(self, username, password):
+        """Auto-generated docstring for function __init__."""
         self.username = username
         self.password = password
 
     def __eq__(self, other):
-        return all(
-            [
-                self.username == getattr(other, 'username', None),
-                self.password == getattr(other, 'password', None),
-            ]
-        )
+        """Auto-generated docstring for function __eq__."""
+        return all([self.username == getattr(other, 'username', None), self.password == getattr(other, 'password', None)])
 
     def __ne__(self, other):
+        """Auto-generated docstring for function __ne__."""
         return not self == other
 
     def __call__(self, r):
-        r.headers['Authorization'] = _basic_auth_str(
-            self.username, self.password
-        )
+        """Auto-generated docstring for function __call__."""
+        r.headers['Authorization'] = _basic_auth_str(self.username, self.password)
         return r
-
 
 class HTTPDigestAuth(AuthBase):
     """Attaches HTTP Digest Authentication to the given Request object."""
 
     def __init__(self, username, password):
+        """Auto-generated docstring for function __init__."""
         self.username = username
         self.password = password
-        # Keep state in per-thread local storage
         self._thread_local = threading.local()
 
     def init_per_thread_state(self):
-        # Ensure state is initialized just once per-thread
+        """Auto-generated docstring for function init_per_thread_state."""
         if not hasattr(self._thread_local, 'init'):
             self._thread_local.init = True
             self._thread_local.last_nonce = ''
@@ -112,32 +91,28 @@ class HTTPDigestAuth(AuthBase):
             _algorithm = 'MD5'
         else:
             _algorithm = algorithm.upper()
-        # lambdas assume digest modules are imported at the top level
         if _algorithm == 'MD5' or _algorithm == 'MD5-SESS':
 
             def md5_utf8(x):
+                """Auto-generated docstring for function md5_utf8."""
                 if isinstance(x, str):
                     x = x.encode('utf-8')
                 return hashlib.md5(x).hexdigest()
-
             hash_utf8 = md5_utf8
         elif _algorithm == 'SHA':
 
             def sha_utf8(x):
+                """Auto-generated docstring for function sha_utf8."""
                 if isinstance(x, str):
                     x = x.encode('utf-8')
                 return hashlib.sha1(x).hexdigest()
-
             hash_utf8 = sha_utf8
-        KD = lambda s, d: hash_utf8("%s:%s" % (s, d))
+        KD = lambda s, d: hash_utf8('%s:%s' % (s, d))
         if hash_utf8 is None:
             return None
-
-        # XXX not implemented yet
         entdig = None
         p_parsed = urlparse(url)
-        # : path is request-uri defined in RFC 2616 which should not be empty
-        path = p_parsed.path or "/"
+        path = p_parsed.path or '/'
         if p_parsed.query:
             path += '?' + p_parsed.query
         A1 = '%s:%s:%s' % (self.username, realm, self.password)
@@ -153,23 +128,18 @@ class HTTPDigestAuth(AuthBase):
         s += nonce.encode('utf-8')
         s += time.ctime().encode('utf-8')
         s += os.urandom(8)
-        cnonce = (hashlib.sha1(s).hexdigest()[:16])
+        cnonce = hashlib.sha1(s).hexdigest()[:16]
         if _algorithm == 'MD5-SESS':
             HA1 = hash_utf8('%s:%s:%s' % (HA1, nonce, cnonce))
         if not qop:
-            respdig = KD(HA1, "%s:%s" % (nonce, HA2))
+            respdig = KD(HA1, '%s:%s' % (nonce, HA2))
         elif qop == 'auth' or 'auth' in qop.split(','):
-            noncebit = "%s:%s:%s:%s:%s" % (nonce, ncvalue, cnonce, 'auth', HA2)
+            noncebit = '%s:%s:%s:%s:%s' % (nonce, ncvalue, cnonce, 'auth', HA2)
             respdig = KD(HA1, noncebit)
         else:
-            # XXX handle auth-int.
             return None
-
         self._thread_local.last_nonce = nonce
-        # XXX should the partial digests be encoded too?
-        base = 'username="%s", realm="%s", nonce="%s", uri="%s", ' 'response="%s"' % (
-            self.username, realm, nonce, path, respdig
-        )
+        base = 'username="%s", realm="%s", nonce="%s", uri="%s", response="%s"' % (self.username, realm, nonce, path, respdig)
         if opaque:
             base += ', opaque="%s"' % opaque
         if algorithm:
@@ -178,7 +148,7 @@ class HTTPDigestAuth(AuthBase):
             base += ', digest="%s"' % entdig
         if qop:
             base += ', qop="auth", nc=%s, cnonce="%s"' % (ncvalue, cnonce)
-        return 'Digest %s' % (base)
+        return 'Digest %s' % base
 
     def handle_redirect(self, r, **kwargs):
         """Reset num_401_calls counter on redirects."""
@@ -191,56 +161,37 @@ class HTTPDigestAuth(AuthBase):
 
         :rtype: requests.Response
         """
-        # If response is not 4xx, do not auth
-        # See https://github.com/requests/requests/issues/3772
         if not 400 <= r.status_code < 500:
             self._thread_local.num_401_calls = 1
             return r
-
         if self._thread_local.pos is not None:
-            # Rewind the file position indicator of the body to where
-            # it was to resend the request.
             r.request.body.seek(self._thread_local.pos)
         s_auth = r.headers.get('www-authenticate', '')
         if 'digest' in s_auth.lower() and self._thread_local.num_401_calls < 2:
             self._thread_local.num_401_calls += 1
-            pat = re.compile(r'digest ', flags=re.IGNORECASE)
-            self._thread_local.chal = parse_dict_header(
-                pat.sub('', s_auth, count=1)
-            )
-            # Consume content and release the original connection
-            # to allow our new request to reuse the same one.
+            pat = re.compile('digest ', flags=re.IGNORECASE)
+            self._thread_local.chal = parse_dict_header(pat.sub('', s_auth, count=1))
             r.content
             r.close()
             prep = r.request.copy()
             extract_cookies_to_jar(prep._cookies, r.request, r.raw)
             prep.prepare_cookies(prep._cookies)
-            prep.headers['Authorization'] = self.build_digest_header(
-                prep.method, prep.url
-            )
+            prep.headers['Authorization'] = self.build_digest_header(prep.method, prep.url)
             _r = r.connection.send(prep, **kwargs)
             _r.history.append(r)
             _r.request = prep
             return _r
-
         self._thread_local.num_401_calls = 1
         return r
 
     def __call__(self, r):
-        # Initialize per-thread state, if needed
+        """Auto-generated docstring for function __call__."""
         self.init_per_thread_state()
-        # If we have a saved nonce, skip the 401
         if self._thread_local.last_nonce:
-            r.headers['Authorization'] = self.build_digest_header(
-                r.method, r.url
-            )
+            r.headers['Authorization'] = self.build_digest_header(r.method, r.url)
         try:
             self._thread_local.pos = r.body.tell()
         except AttributeError:
-            # In the case of HTTPDigestAuth being reused and the body of
-            # the previous request was a file-like object, pos has the
-            # file position of the previous body. Ensure it's set to
-            # None.
             self._thread_local.pos = None
         r.register_hook('response', self.handle_401)
         r.register_hook('response', self.handle_redirect)
@@ -248,12 +199,9 @@ class HTTPDigestAuth(AuthBase):
         return r
 
     def __eq__(self, other):
-        return all(
-            [
-                self.username == getattr(other, 'username', None),
-                self.password == getattr(other, 'password', None),
-            ]
-        )
+        """Auto-generated docstring for function __eq__."""
+        return all([self.username == getattr(other, 'username', None), self.password == getattr(other, 'password', None)])
 
     def __ne__(self, other):
+        """Auto-generated docstring for function __ne__."""
         return not self == other
