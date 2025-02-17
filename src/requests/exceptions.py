6d3f3bd98cc4f37082478401f8f52676420f889e
NicolleLouis <louisxnicolle@gmail.com>
2025-02-17 18:45:10 +0100

    Normalize RequestException init

--- src/requests/exceptions.py
+++ src/requests/exceptions.py
@@ -16,8 +16,7 @@ class RequestException(IOError):
 
     def __init__(self, *args, **kwargs):
         """Initialize RequestException with `request` and `response` objects."""
-        response = kwargs.pop("response", None)
-        self.response = response
+        self.response = kwargs.pop("response", None)
         self.request = kwargs.pop("request", None)
         if response is not None and not self.request and hasattr(response, "request"):
             self.request = self.response.request
