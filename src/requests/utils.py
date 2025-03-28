0aceb9f98677b35e01a99af36c9caf1e7102b479
Benedikt McMullin <benedikt@mcmullin.de>
2025-03-29 00:11:12 +0100

    Don't normalise or double-escape urls
    `requote_uri` was added in 2013 in #1361 to resolve #1360.
    
    But in 2019 this was also solved in `urllib3` in
    https://github.com/urllib3/urllib3/pull/1647.
    
    Not only does the `urllib3` implementation more closely match what
    all other clients are doing.  It also means that we are double-encoding
    invalid uris.  If the redirect is invalid we're in unchartered territory
    but the path that `requests` uses has no chance to decode to the same
    that any other client uses, even to the most lenient server.
    
    Simply drop `requote_uri`, as `urllib3`'s `parse_url` will always handle
    invalid urls.

--- src/requests/utils.py
+++ src/requests/utils.py
@@ -630,58 +630,6 @@ def get_unicode_from_response(r):
         return r.content
 
 
-# The unreserved URI characters (RFC 3986)
-UNRESERVED_SET = frozenset(
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" + "0123456789-._~"
-)
-
-
-def unquote_unreserved(uri):
-    """Un-escape any percent-escape sequences in a URI that are unreserved
-    characters. This leaves all reserved, illegal and non-ASCII bytes encoded.
-
-    :rtype: str
-    """
-    parts = uri.split("%")
-    for i in range(1, len(parts)):
-        h = parts[i][0:2]
-        if len(h) == 2 and h.isalnum():
-            try:
-                c = chr(int(h, 16))
-            except ValueError:
-                raise InvalidURL(f"Invalid percent-escape sequence: '{h}'")
-
-            if c in UNRESERVED_SET:
-                parts[i] = c + parts[i][2:]
-            else:
-                parts[i] = f"%{parts[i]}"
-        else:
-            parts[i] = f"%{parts[i]}"
-    return "".join(parts)
-
-
-def requote_uri(uri):
-    """Re-quote the given URI.
-
-    This function passes the given URI through an unquote/quote cycle to
-    ensure that it is fully and consistently quoted.
-
-    :rtype: str
-    """
-    safe_with_percent = "!#$%&'()*+,/:;=?@[]~"
-    safe_without_percent = "!#$&'()*+,/:;=?@[]~"
-    try:
-        # Unquote only the unreserved characters
-        # Then quote only illegal characters (do not quote reserved,
-        # unreserved, or '%')
-        return quote(unquote_unreserved(uri), safe=safe_with_percent)
-    except InvalidURL:
-        # We couldn't unquote the given URI, so let's try quoting it, but
-        # there may be unquoted '%'s in the URI. We need to make sure they're
-        # properly quoted so they do not cause issues elsewhere.
-        return quote(uri, safe=safe_without_percent)
-
-
 def address_in_network(ip, net):
     """This function allows you to check if an IP belongs to a network subnet
 
