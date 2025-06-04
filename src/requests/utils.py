34799a59b82428f62a68857991d1e89ed462cf35
Arthur Woimbée <arthur.woimbee@gmail.com>
2025-06-04 12:36:54 +0200

    fix CVE-2024-47081

--- src/requests/utils.py
+++ src/requests/utils.py
@@ -236,16 +236,8 @@ def get_netrc_auth(url, raise_errors=False):
             return
 
         ri = urlparse(url)
-
-        # Strip port numbers from netloc. This weird `if...encode`` dance is
-        # used for Python 3.2, which doesn't support unicode literals.
-        splitstr = b":"
-        if isinstance(url, str):
-            splitstr = splitstr.decode("ascii")
-        host = ri.netloc.split(splitstr)[0]
-
         try:
-            _netrc = netrc(netrc_path).authenticators(host)
+            _netrc = netrc(netrc_path).authenticators(ri.hostname)
             if _netrc:
                 # Return with login / password
                 login_i = 0 if _netrc[0] else 1
