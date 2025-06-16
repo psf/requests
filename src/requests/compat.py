a42d58462bebc03f257ab52933af0af9b9970eee
Javier Buzzi <buzzi.javier@gmail.com>
2025-06-16 08:15:08 -0400

    Update src/requests/compat.py

--- src/requests/compat.py
+++ src/requests/compat.py
@@ -65,7 +65,6 @@ from collections.abc import Callable, Mapping, MutableMapping
 from http import cookiejar as cookielib
 from http.cookies import Morsel
 from io import StringIO
-from json import JSONDecodeError
 
 # --------------
 # Legacy Imports
