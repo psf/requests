31e811f8ceb24f019d4bcb2dd9896199e57bbdef
Javier Buzzi <buzzi.javier@gmail.com>
2025-06-16 07:49:09 -0400

    Removes py2 code

--- src/requests/compat.py
+++ src/requests/compat.py
@@ -45,15 +45,6 @@ chardet = _resolve_char_detection()
 # Pythons
 # -------
 
-# Syntax sugar.
-_ver = sys.version_info
-
-#: Python 2.x?
-is_py2 = _ver[0] == 2
-
-#: Python 3.x?
-is_py3 = _ver[0] == 3
-
 # json/simplejson module import resolution
 has_simplejson = False
 try:
