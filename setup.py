37a7d0f7317c764c3910b40158be09d28ef597d5
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-24 15:20:01 -0700

    Add trove classifier for Python 3.14

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
