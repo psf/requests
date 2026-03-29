5c93c71069a65381a4722dda9df4210e6d381240
Nate Prewitt <nate.prewitt@gmail.com>
2026-03-28 22:51:25 -0600

    Make length positional like io.Reader

--- src/requests/_types.py
+++ src/requests/_types.py
@@ -23,7 +23,7 @@ _T_co = TypeVar("_T_co", covariant=True)
 
 @runtime_checkable
 class SupportsRead(Protocol[_T_co]):
-    def read(self, length: int = ...) -> _T_co: ...
+    def read(self, length: int = ..., /) -> _T_co: ...
 
 
 @runtime_checkable
