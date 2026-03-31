96ef1dec123e8ecef55e77fad36f783d99d37bdc
brian <github@orbitfrost.com>
2026-03-31 18:32:38 -0400

    Preserve leading slashes in request path_url
    Fixes issue where S3 presigned URLs with keys starting with '/'
    were incorrectly modified, breaking URL signatures.
    
    URLs like 'https://bucket.s3.amazonaws.com//key_name' now
    correctly preserve '//key_name' in the path_url.
    
    Closes #6711

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,8 +1,14 @@
 import requests.adapters
 
 
-def test_request_url_trims_leading_path_separators():
-    """See also https://github.com/psf/requests/issues/6643."""
+def test_request_url_preserves_leading_path_separators():
+    """See also https://github.com/psf/requests/issues/6711.
+    
+    S3 presigned URLs with keys starting with '/' produce paths like
+    '//key_name'. We should preserve leading slashes to avoid breaking signatures.
+    """
     a = requests.adapters.HTTPAdapter()
-    p = requests.Request(method="GET", url="http://127.0.0.1:10000//v:h").prepare()
-    assert "/v:h" == a.request_url(p, {})
+    p = requests.Request(
+        method="GET", url="https://bucket.s3.amazonaws.com//key_with_leading_slash.txt"
+    ).prepare()
+    assert "//key_with_leading_slash.txt" == a.request_url(p, {})
