23789d6b7d335167a6a67460bf8234af337a3b40
杨兴隆 <81056572+717986230@users.noreply.github.com>
2026-04-03 20:06:39 +0800

    Fix #7122: Preserve cookie policy in RequestsCookieJar.copy()

--- src/requests/cookies.py
+++ src/requests/cookies.py
@@ -427,7 +427,7 @@ class RequestsCookieJar(cookielib.CookieJar, MutableMapping):
 
     def copy(self):
         """Return a copy of this RequestsCookieJar."""
-        new_cj = RequestsCookieJar()
+        new_cj = RequestsCookieJar(self._policy)
         new_cj.set_policy(self.get_policy())
         new_cj.update(self)
         return new_cj
