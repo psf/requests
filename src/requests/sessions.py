cd90742ed94d901759e26766197d0ce7c7bd9c8e
aldbr <aldbr@outlook.com>
2026-05-18 17:02:35 +0200

    docs: indent continuation line in Session.get :param params: (#7460)

--- src/requests/sessions.py
+++ src/requests/sessions.py
@@ -662,7 +662,7 @@ class Session(SessionRedirectMixin):
 
         :param url: URL for the new :class:`Request` object.
         :param params: (optional) Dictionary, list of tuples or bytes to send
-        in the query string for the :class:`Request`.
+            in the query string for the :class:`Request`.
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
